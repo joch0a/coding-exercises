e36f668cf030248faa0c85019f8fd79d8e5d09fe
Jose Ochoa <JOchoa@intouchhealth.com>
2020-12-27 21:59:59 -0300

    Is subsequence

---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterviewPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        // https://leetcode.com/problems/is-subsequence/
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            var i = 0;
+            var j = 0;
+
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == s[j])
+                {
+                    i++;
+                }
+
+                j++;
+            }
+
+            return i == s.Length;
+        }
+    }
+}
