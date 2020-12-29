6d52e030774966e0a0cb2dcc3f11cfbd87f58093
Jose Ochoa <JOchoa@intouchhealth.com>
2020-12-29 02:10:28 -0300

    HasPathSum

--- InterviewPreparation/Program.cs
+++ InterviewPreparation/Program.cs
@@ -31,30 +31,30 @@ namespace InterviewPreparation
             return i == s.Length;
         }
 
-        static int maxDiff(Node root, int k)
-        {
-            int min = Integer.MAX_VALUE;
+        //static int maxDiff(Node root, int k)
+        //{
+        //    int min = Integer.MAX_VALUE;
 
-            while (root != null)
-            {
-                min = Math.min(min, Math.abs(root.data - k));
+        //    while (root != null)
+        //    {
+        //        min = Math.min(min, Math.abs(root.data - k));
 
-                if (k > root.data)
-                {
-                    root = root.right;
-                }
-                else if (k < root.data)
-                {
-                    root = root.left;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
+        //        if (k > root.data)
+        //        {
+        //            root = root.right;
+        //        }
+        //        else if (k < root.data)
+        //        {
+        //            root = root.left;
+        //        }
+        //        else
+        //        {
+        //            return 0;
+        //        }
+        //    }
 
-            return min;
-        }
+        //    return min;
+        //}
 
         // https://practice.geeksforgeeks.org/problems/find-the-closest-element-in-bst/1#
         //class Solution
@@ -80,10 +80,52 @@ namespace InterviewPreparation
         //            {
         //                return 0;
         //            }
-        //        }
+        //        }     
+        /**/
+        //* Definition for a binary tree node.
 
-        //        return min;
-        //    }
-        //}
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        /**
+         * Definition for a binary tree node.
+         * public class TreeNode {
+         *     public int val;
+         *     public TreeNode left;
+         *     public TreeNode right;
+         *     public TreeNode(int x) { val = x; }
+         * }
+         *  var node1 = new TreeNode(7);
+            var node2 = new TreeNode(2);
+            var node3 = new TreeNode(11, node1, node2);
+            var node4 = new TreeNode(4, node3);
+            var node5 = new TreeNode(5, node4);
+
+            var result = HasPathSum(node5, 22);
+         */
+
+        // https://leetcode.com/problems/path-sum/submissions/
+        public class Solution
+        {
+            public bool HasPathSum(TreeNode root, int sum)
+            {
+                if (root == null)
+                    return false;
+                else if (root.left == null && root.right == null && root.val == sum)
+                    return true;
+                else
+                    return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+            }
+        }
     }
 }
