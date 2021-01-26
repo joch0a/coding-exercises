ef66ce0a6f5ce98cfac2a0ca1bef6ae0ddc07ac6
Jose Ochoa <JOchoa@intouchhealth.com>
2021-01-25 23:07:27 -0300

    MoveZeros

---
+++ InterviewPreparation/Exercises/MoveZeros.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var fillIndex = -1;
+
+        int i = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (fillIndex == -1)
+            {
+                fillIndex = nums[i] == 0 ? i : -1;
+            }
+            else if (i != fillIndex && nums[i] != 0)
+            {
+                nums[fillIndex] = nums[i];
+                fillIndex++;
+                nums[i] = 0;
+            }
+        }
+    }
+}
