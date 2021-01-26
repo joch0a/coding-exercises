ed40a2b1c34389253647e43b22314f07e9079a14
Jose Ochoa <JOchoa@intouchhealth.com>
2021-01-25 23:32:26 -0300

    Refactor three sum

--- InterviewPreparation/Exercises/MoveZeros.cs
+++ InterviewPreparation/Exercises/MoveZeros.cs
@@ -1,22 +1,23 @@
 ﻿namespace InterviewPreparation.Exercises
 {
-    public void MoveZeroes(int[] nums)
+    public class MoveZeroesClass
     {
-        var fillIndex = -1;
-
-        int i = 0;
-
-        for (int i = 0; i < nums.Length; i++)
+        public void MoveZeroes(int[] nums)
         {
-            if (fillIndex == -1)
+            var fillIndex = -1;
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                fillIndex = nums[i] == 0 ? i : -1;
-            }
-            else if (i != fillIndex && nums[i] != 0)
-            {
-                nums[fillIndex] = nums[i];
-                fillIndex++;
-                nums[i] = 0;
+                if (fillIndex == -1)
+                {
+                    fillIndex = nums[i] == 0 ? i : -1;
+                }
+                else if (i != fillIndex && nums[i] != 0)
+                {
+                    nums[fillIndex] = nums[i];
+                    fillIndex++;
+                    nums[i] = 0;
+                }
             }
         }
     }
---
+++ InterviewPreparation/Exercises/ThreeSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.Exercises
+{
+    public class ThreeSumClass
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> tuples = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+                {
+                    var low = i + 1;
+                    var high = nums.Length - 1;
+                    var sum = -nums[i];
+
+                    while (low < high)
+                    {
+                        if (nums[low] + nums[high] == sum)
+                        {
+                            tuples.Add(new int[] { nums[i], nums[low], nums[high] });
+
+                            while (low < high && nums[low] == nums[low + 1])
+                            {
+                                low++;
+                            }
+
+                            while (low < high && nums[high] == nums[high - 1])
+                            {
+                                high--;
+                            }
+
+                            low++;
+                            high--;
+                        }
+                        else if (nums[low] + nums[high] > sum)
+                        {
+                            high--;
+                        }
+                        else
+                        {
+                            low++;
+                        }
+                    }
+                }
+            }
+
+            return tuples;
+        }
+    }
+}
