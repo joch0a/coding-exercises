1048c20667bc68ab987593d8e7b9ee95c6c7e97e
Jose Ochoa <JOchoa@intouchhealth.com>
2021-01-26 00:39:51 -0300

    isMonotonic

---
+++ InterviewPreparation/Exercises/Monotonic.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public class Monotonic
+    {
+        public bool isMonotonic(int[] A)
+        {
+            bool increasing = true;
+            bool decreasing = true;
+
+            for (int i = 0; i < A.Length - 1; ++i)
+            {
+                if (A[i] > A[i + 1])
+                    increasing = false;
+                if (A[i] < A[i + 1])
+                    decreasing = false;
+            }
+
+            return increasing || decreasing;
+        }
+    }
+}
