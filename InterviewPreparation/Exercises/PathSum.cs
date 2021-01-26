422ccc62e1fa95119b72f96f4e62bfe329176695
Jose Ochoa <JOchoa@intouchhealth.com>
2021-01-25 23:48:57 -0300

    SubSequence refactor

---
+++ InterviewPreparation/Exercises/PathSum.cs
@@ -0,0 +1,32 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    class PathSum
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        // https://leetcode.com/problems/path-sum/submissions/
+        public class Solution
+        {
+            public bool HasPathSum(TreeNode root, int sum)
+            {
+                if (root == null)
+                    return false;
+                else if (root.left == null && root.right == null && root.val == sum)
+                    return true;
+                else
+                    return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+            }
+        }
+    }
+}
---
+++ InterviewPreparation/Exercises/SubSequenceCheck.cs
@@ -0,0 +1,27 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    class SubSequenceCheck
+    {
+
+        // problem: https://leetcode.com/problems/is-subsequence/
+        // cool video: https://www.youtube.com/watch?v=UsPWwTY0xr4&ab_channel=NareshGupta
+
+        public static bool IsSubsequence(string s, string t)
+        {
+            var i = 0;
+            var j = 0;
+
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == s[j])
+                {
+                    i++;
+                }
+
+                j++;
+            }
+
+            return i == s.Length;
+        }
+    }
+}
