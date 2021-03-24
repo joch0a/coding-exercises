52648005773fe9d6a79384127abe76372ebf8642
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-23 23:32:47 -0300

    ValidateBST recursive

---
+++ InterviewPreparation/Exercises/ValidateBST.cs
@@ -0,0 +1,26 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public class ValidateBST
+    {
+        //Recursive way
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsValidBST(root, null, null);
+        }
+
+        private bool IsValidBST(TreeNode root, int? min, int? max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if ((min != null && min >= root.val) || (max != null && max <= root.val))
+            {
+                return false;
+            }
+
+            return IsValidBST(root.left, min, root.val) && IsValidBST(root.right, root.val, max);
+        }
+    }
+}
