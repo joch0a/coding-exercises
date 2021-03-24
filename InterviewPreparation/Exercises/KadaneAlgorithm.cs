3f7cd3a9f6c8656520470ff5bbfd21b597009019
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-23 23:51:23 -0300

    Largest Sum Contiguous Subarray

---
+++ InterviewPreparation/Exercises/KadaneAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public class KadaneAlgorithm
+    {
+        // Largest Sum Contiguous Subarray
+        public int KadanesAlgorithm(int[] array)
+        {
+            var currentMax = array[0];
+            var globalMax = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                var num = array[i];
+                currentMax = Math.Max(num, currentMax + num);
+                globalMax = Math.Max(globalMax, currentMax);
+            }
+
+            return globalMax;
+        }
+    }
+}
