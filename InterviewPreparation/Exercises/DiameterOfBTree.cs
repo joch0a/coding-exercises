e6daabf7015d13b8a80194d722af3bc5d2f47ff0
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-24 22:37:57 -0300

    DiameterOfBinaryTree

---
+++ InterviewPreparation/Exercises/DiameterOfBTree.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public class DiameterOfBTree
+    {
+        private int diameter;
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            diameter = 0;
+
+            LongestPath(root);
+
+            return diameter;
+        }
+
+        public int LongestPath(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftDepth = LongestPath(root.left);
+            var rightDepth = LongestPath(root.right);
+
+            diameter = Math.Max(diameter, leftDepth + rightDepth);
+
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
