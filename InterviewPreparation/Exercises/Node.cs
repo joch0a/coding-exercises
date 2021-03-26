296b244d62b9f6069abd5e212f675a388c60b3f3
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-25 21:15:01 -0300

    Refactor node

---
+++ InterviewPreparation/Exercises/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace InterviewPreparation.Exercises
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+        public Node next;
+        Node(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}
--- InterviewPreparation/KthLargestInBST.cs
+++ InterviewPreparation/KthLargestInBST.cs
@@ -2,18 +2,7 @@
 
 namespace InterviewPreparation
 {
-    public class Node
-    {
-        public int data;
-        public Node left;
-        public Node right;
-        Node(int data)
-        {
-            this.data = data;
-            left = null;
-            right = null;
-        }
-    }
+
 
     public class KthLargestInBST
     {
