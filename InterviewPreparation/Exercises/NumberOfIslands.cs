21b3d438e19649d7e03a2f6e02a98d769631f767
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-26 01:13:54 -0300

    Re-visit number of islands

--- InterviewPreparation/Exercises/NumberOfIslands.cs
+++ InterviewPreparation/Exercises/NumberOfIslands.cs
@@ -5,22 +5,23 @@ namespace InterviewPreparation.Exercises
 {
     public class NumberOfIslands
     {
-        public static int NumIslands(char[][] grid)
+        public int NumIslands(char[][] grid)
         {
+            if (grid == null)
+            {
+                return 0;
+            }
+
             var numberOfIslands = 0;
 
-            if (grid.Length > 0)
+            for (int i = 0; i < grid.GetLength(0); i++)
             {
-                var visited = new bool[grid.Length, grid[0].Length];
-
-                for (var i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[i].GetLength(0); j++)
                 {
-                    for (var j = 0; j < grid[i].Length; j++)
+                    if (grid[i][j] == '1')
                     {
-                        if (!visited[i, j])
-                        {
-                            explore(grid, visited, ref numberOfIslands, i, j);
-                        }
+                        visitIsland(grid, i, j);
+                        numberOfIslands++;
                     }
                 }
             }
@@ -28,106 +29,44 @@ namespace InterviewPreparation.Exercises
             return numberOfIslands;
         }
 
-        private static void explore(char[][] grid, bool[,] visited, ref int numberOfIslands, int i, int j)
+        public void visitIsland(char[][] grid, int i, int j)
         {
-            var nodesToVisit = new Stack<Tuple<int, int>>();
-            var islandFound = false;
+            var queue = new Queue<Tuple<int, int>>();
 
-            nodesToVisit.Push(new Tuple<int, int>(i, j));
+            queue.Enqueue(new Tuple<int, int>(i, j));
 
-            while (nodesToVisit.Count > 0)
+            while (queue.Count > 0)
             {
-                var actualNode = nodesToVisit.Pop();
-
-                if (!visited[actualNode.Item1, actualNode.Item2])
+                var actual = queue.Dequeue();
+                if (grid[actual.Item1][actual.Item2] == '1')
                 {
-                    visited[actualNode.Item1, actualNode.Item2] = true;
-
-                    if (grid[actualNode.Item1][actualNode.Item2] == '1')
-                    {
-                        islandFound = true;
-                        var neighbours = GetNeighbours(grid, visited, actualNode.Item1, actualNode.Item2);
-
-                        foreach (var neighbour in neighbours)
-                        {
-                            nodesToVisit.Push(neighbour);
-                        }
-                    }
+                    grid[actual.Item1][actual.Item2] = '0';
+                    calculateNeighbours(grid, queue, actual.Item1, actual.Item2);
                 }
             }
-
-            if (islandFound)
-            {
-                numberOfIslands++;
-            }
         }
 
-        private static IEnumerable<Tuple<int, int>> GetNeighbours(char[][] grid, bool[,] visited, int i, int j)
+        public void calculateNeighbours(char[][] grid, Queue<Tuple<int, int>> queue, int i, int j)
         {
-            var neighbours = new List<Tuple<int, int>>();
-
-            if (i > 0 && !visited[i - 1, j])
+            if (i > 0 && grid[i - 1][j] == '1')
             {
-                neighbours.Add(new Tuple<int, int>(i - 1, j));
+                queue.Enqueue(new Tuple<int, int>(i - 1, j));
             }
 
-            if (j > 0 && !visited[i, j - 1])
+            if (i < grid.Length - 1 && grid[i + 1][j] == '1')
             {
-                neighbours.Add(new Tuple<int, int>(i, j - 1));
+                queue.Enqueue(new Tuple<int, int>(i + 1, j));
             }
 
-            if (i < grid.Length - 1 && !visited[i + 1, j])
+            if (j > 0 && grid[i][j - 1] == '1')
             {
-                neighbours.Add(new Tuple<int, int>(i + 1, j));
+                queue.Enqueue(new Tuple<int, int>(i, j - 1));
             }
 
-            if (j < grid[i].Length - 1 && !visited[i, j + 1])
+            if (j < grid[i].Length - 1 && grid[i][j + 1] == '1')
             {
-                neighbours.Add(new Tuple<int, int>(i, j + 1));
+                queue.Enqueue(new Tuple<int, int>(i, j + 1));
             }
-
-            return neighbours;
         }
-
-        //Code from Leetcode more optimized
-        //public int NumIslands(char[][] grid)
-        //{
-
-        //    if (grid == null || grid.Length == 0)
-        //    {
-        //        return 0;
-        //    }
-
-        //    int numberOfIslands = 0;
-
-        //    for (int i = 0; i < grid.GetLength(0); i++)
-        //    {
-        //        for (int j = 0; j < grid[i].Length; j++)
-        //        {
-        //            char v = grid[i][j];
-        //            if (v == '1')
-        //            {
-        //                numberOfIslands += sink(grid, i, j);
-        //            }
-        //        }
-        //    }
-
-        //    return numberOfIslands;
-        //}
-
-        //public int sink(char[][] grid, int i, int j)
-        //{
-        //    if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] == '0')
-        //    {
-        //        return 0;
-        //    }
-
-        //    grid[i][j] = '0';
-        //    sink(grid, i, j + 1);
-        //    sink(grid, i, j - 1);
-        //    sink(grid, i + 1, j);
-        //    sink(grid, i - 1, j);
-        //    return 1;
-        //}
     }
 }
