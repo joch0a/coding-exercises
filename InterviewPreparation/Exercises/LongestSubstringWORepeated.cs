18d3ee6a7b43e3c325269099afccf0b0c28dabf2
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-27 14:56:52 -0300

    longest-substring-without-repeating-characters FIRST ACCEPTED APPROACH

---
+++ InterviewPreparation/Exercises/LongestSubstringWORepeated.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.Exercises
+{
+    class LongestSubstringWORepeated
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var max = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var visitedChars = new HashSet<char>();
+                var foundRepeated = false;
+                var actualCount = 0;
+
+                for (int j = i; j < s.Length && !foundRepeated; j++)
+                {
+                    if (!visitedChars.Contains(s[j]))
+                    {
+                        visitedChars.Add(s[j]);
+                        actualCount++;
+                        max = Math.Max(max, actualCount);
+                    }
+                    else
+                    {
+                        foundRepeated = true;
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
