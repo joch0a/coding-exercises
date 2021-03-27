625aa69670fcabf830efe367bb426119a0d1f6c2
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-27 05:30:35 -0300

    First accepted approach group anagrams

---
+++ InterviewPreparation/Exercises/GroupAnagramsSolved.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.Exercises
+{
+    class GroupAnagramsSolved
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var anagrams = new List<IList<string>>();
+            var visited = new HashSet<int>();
+            var buckets = new List<KeyValuePair<string, int[]>>();
+
+            foreach (var str in strs)
+            {
+                var actualBucket = new int[27];
+
+                for (var i = 0; i < str.Length; i++)
+                {
+                    actualBucket[str[i] - 'a']++;
+                }
+
+                buckets.Add(new KeyValuePair<string, int[]>(str, actualBucket));
+            }
+
+            for (int i = 0; i < buckets.Count; i++)
+            {
+                var keyPair = buckets[i];
+
+                if (!visited.Contains(i))
+                {
+                    var groupAnagram = new List<string>();
+                    groupAnagram.Add(keyPair.Key);
+
+                    visited.Add(i);
+
+                    for (int j = i + 1; j < buckets.Count; j++)
+                    {
+                        var actualKeyPair = buckets[j];
+
+                        if (!visited.Contains(j) && IsAnagram(keyPair.Value, actualKeyPair.Value))
+                        {
+                            groupAnagram.Add(actualKeyPair.Key);
+                            visited.Add(j);
+                        }
+                    }
+
+                    anagrams.Add(groupAnagram);
+                }
+            }
+
+            return anagrams;
+        }
+
+        private bool IsAnagram(int[] bucket1, int[] bucket2)
+        {
+            for (int i = 0; i < bucket1.Length; i++)
+            {
+                if (bucket1[i] != bucket2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
