b57feef39aaf776e8d048485e85bb3899a100e2f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-27 16:27:21 -0300

    Add OptimizedLengthOfLongestSubstring solution using sliding window technique

--- InterviewPreparation/Exercises/LongestSubstringWORepeated.cs
+++ InterviewPreparation/Exercises/LongestSubstringWORepeated.cs
@@ -32,5 +32,36 @@ namespace InterviewPreparation.Exercises
 
             return max;
         }
+
+        public int OptimizedLengthOfLongestSubstring(string s)
+        {
+            var positions = new Dictionary<char, int>();
+            var max = 0;
+            var i = 0;
+
+            for (int j = 0; j < s.Length; j++)
+            {
+                var alreadyVisitedChar = positions.ContainsKey(s[j]);
+
+                if (alreadyVisitedChar)
+                {
+                    i = Math.Max(positions[s[j]], i);
+                }
+
+                max = Math.Max(max, j - i + 1);
+
+                if (alreadyVisitedChar)
+                {
+                    positions[s[j]] = j + 1;
+                }
+                else
+                {
+                    positions.Add(s[j], j + 1);
+                }
+            }
+
+            return max;
+        }
     }
+}
 }
