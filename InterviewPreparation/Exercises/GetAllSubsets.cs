aeb949f83b55f89f2e06dc0bd79289f2d0f8615f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-29 09:27:11 -0300

    Subsets

---
+++ InterviewPreparation/Exercises/GetAllSubsets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.Exercises
+{
+    class GetAllSubsets
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var subsets = new List<IList<int>>();
+
+            GenerateSubsets(new List<int>(), 0, subsets, nums);
+
+            return subsets;
+        }
+
+        private void GenerateSubsets(IList<int> actual, int index, IList<IList<int>> subsets, int[] nums)
+        {
+            subsets.Add(new List<int>(actual));
+
+            for (int i = index; i < nums.Length; i++)
+            {
+                actual.Add(nums[i]);
+
+                GenerateSubsets(actual, i + 1, subsets, nums);
+
+                actual.RemoveAt(actual.Count - 1);
+            }
+        }
+    }
+}
