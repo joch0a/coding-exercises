fbbc88bc14b71ad3fdcc1e1bd53fc1a4a92afd1f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-03-29 18:43:03 -0300

    WordSearch improved. Removed the visited matrix, modify in-place the matrix

--- InterviewPreparation/Exercises/WordSearch.cs
+++ InterviewPreparation/Exercises/WordSearch.cs
@@ -4,17 +4,8 @@ namespace InterviewPreparation.Exercises
 {
     class WordSearch
     {
-        //Fist approach 46/47 tests need to improve
-
         public bool Exist(char[][] board, string word)
         {
-            var visited = new bool[board.Length][];
-
-            for (int i = 0; i < board.Length; i++)
-            {
-                visited[i] = new bool[board[0].Length];
-            }
-
             var exist = false;
 
             for (int i = 0; i < board.Length && !exist; i++)
@@ -23,14 +14,15 @@ namespace InterviewPreparation.Exercises
                 {
                     if (board[i][j] == word[0])
                     {
-                        visited[i][j] = true;
+                        var tmp = board[i][j];
+                        board[i][j] = '*';
 
-                        if (Exist(visited, board, word, 1, i, j))
+                        if (Exist(board, word, 1, i, j))
                         {
                             return true;
                         }
 
-                        visited[i][j] = false;
+                        board[i][j] = tmp;
                     }
                 }
             }
@@ -38,8 +30,7 @@ namespace InterviewPreparation.Exercises
             return false;
         }
 
-        private bool Exist(bool[][] visited,
-                           char[][] board,
+        private bool Exist(char[][] board,
                            string word,
                            int index,
                            int i,
@@ -50,44 +41,44 @@ namespace InterviewPreparation.Exercises
                 return true;
             }
 
-            var neighbours = GetNeighbours(i, j, visited, board, index, word);
+            var neighbours = GetNeighbours(i, j, board, index, word);
 
             foreach (var neighbour in neighbours)
             {
-                visited[neighbour[0]][neighbour[1]] = true;
+                var tmp = board[neighbour[0]][neighbour[1]];
+                board[neighbour[0]][neighbour[1]] = '*';
 
-                if (Exist(visited, board, word, index + 1, neighbour[0], neighbour[1]))
+                if (Exist(board, word, index + 1, neighbour[0], neighbour[1]))
                 {
                     return true;
                 }
 
-                visited[neighbour[0]][neighbour[1]] = false;
+                board[neighbour[0]][neighbour[1]] = tmp;
             }
 
             return false;
         }
 
-        private List<int[]> GetNeighbours(int i, int j, bool[][] visited,
-                           char[][] board, int index, string word)
+        private List<int[]> GetNeighbours(int i, int j, char[][] board, int index, string word)
         {
             List<int[]> neighbours = new List<int[]>();
 
-            if (i > 0 && !visited[i - 1][j] && board[i - 1][j] == word[index])
+            if (i > 0 && board[i - 1][j] == word[index])
             {
                 neighbours.Add(new int[] { i - 1, j });
             }
 
-            if (j > 0 && !visited[i][j - 1] && board[i][j - 1] == word[index])
+            if (j > 0 && board[i][j - 1] == word[index])
             {
                 neighbours.Add(new int[] { i, j - 1 });
             }
 
-            if (i < board.Length - 1 && !visited[i + 1][j] && board[i + 1][j] == word[index])
+            if (i < board.Length - 1 && board[i + 1][j] == word[index])
             {
                 neighbours.Add(new int[] { i + 1, j });
             }
 
-            if (j < board[0].Length - 1 && !visited[i][j + 1] && board[i][j + 1] == word[index])
+            if (j < board[0].Length - 1 && board[i][j + 1] == word[index])
             {
                 neighbours.Add(new int[] { i, j + 1 });
             }
