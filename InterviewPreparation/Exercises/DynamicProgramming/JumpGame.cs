5973cb21a6eca4abe70701fe5c6f81af5115b5cf
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-04 16:08:22 -0300

    JumpGame

---
+++ InterviewPreparation/Exercises/DynamicProgramming/JumpGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InterviewPreparation.Exercises.DynamicProgramming
+{
+    public class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return false;
+            }
+
+            var maxJumps = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (i == 0)
+                {
+                    maxJumps[0] = nums[0];
+                }
+                else
+                {
+                    maxJumps[i] = Math.Max(maxJumps[i - 1] - 1, nums[i]);
+                }
+
+                if (maxJumps[i] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+}
