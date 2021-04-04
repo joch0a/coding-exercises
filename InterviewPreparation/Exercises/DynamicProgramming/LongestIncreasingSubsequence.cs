65190388cb6a137c40c4210dcaf47507ff6bc453
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-04 19:35:30 -0300

    readme

---
+++ InterviewPreparation/Exercises/DynamicProgramming/LongestIncreasingSubsequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InterviewPreparation.Exercises.DynamicProgramming
+{
+    class LongestIncreasingSubsequence
+    {
+        // o(n2) time     o(n) space
+        public int LengthOfLIS(int[] nums)
+        {
+            var LIS = new int[nums.Length];
+            var max = 1;
+
+            Array.Fill(LIS, 1);
+
+            for (int i = 1; i < LIS.Length; i++) 
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    if (nums[i] > nums[j]) 
+                    {
+                        LIS[i]++;
+                        max = Math.Max(max, LIS[i]);
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
