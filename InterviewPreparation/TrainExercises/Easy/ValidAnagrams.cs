e75b4f96dbd402fb9dc90230695288d38163a0e7
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-05 22:20:49 -0300

    Anagrams

---
+++ InterviewPreparation/TrainExercises/Easy/ValidAnagrams.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPreparation.TrainExercises.Easy
+{
+    class ValidAnagrams
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            int[] bucket = new int[26];
+
+            foreach (var character in s)
+            {
+                bucket[character - 'a']++;
+            }
+
+            foreach (var character in t)
+            {
+                bucket[character - 'a']--;
+            }
+
+            foreach (var b in bucket)
+            {
+                if (b != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
