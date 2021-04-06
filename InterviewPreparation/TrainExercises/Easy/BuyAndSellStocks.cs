b4cb6aa430e131de3c441b3cefc1419089dd3ac4
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-05 22:15:27 -0300

    Refactor a little bit the folder structure, add train exercises

---
+++ InterviewPreparation/TrainExercises/Easy/BuyAndSellStocks.cs
@@ -0,0 +1,35 @@
+﻿namespace InterviewPreparation.TrainExercises.Easy
+{
+    public class BuyAndSellStocks
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 1)
+            {
+                return 0;
+            }
+
+            int buy = prices[0];
+            int sell = prices[0];
+            var profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < sell) //time to sell
+                {
+                    profit += sell - buy;
+                    buy = prices[i];
+                }
+
+                sell = prices[i];
+            }
+
+            if (buy < sell)
+            {
+                profit += sell - buy;
+            }
+
+            return profit;
+        }
+    }
+}
