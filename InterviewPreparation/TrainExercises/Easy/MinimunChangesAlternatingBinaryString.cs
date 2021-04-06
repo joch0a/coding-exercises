35048f87f927ca4d14ea22febaecefb67a6b3216
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-06 00:09:52 -0300

    Minimum Changes To Make Alternating Binary String

---
+++ InterviewPreparation/TrainExercises/Easy/MinimunChangesAlternatingBinaryString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterviewPreparation.TrainExercises.Easy
+{
+    // https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+    class MinimunChangesAlternatingBinaryString
+    {
+        public int MinOperations(string s)
+        {
+            var swapsOne = 0;
+            var swapsTwo = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (s[i] == '1')
+                    {
+                        swapsOne++;
+                    }
+                    else
+                    {
+                        swapsTwo++;
+                    }
+                }
+                else
+                {
+                    if (s[i] == '0')
+                    {
+                        swapsOne++;
+                    }
+                    else
+                    {
+                        swapsTwo++;
+                    }
+                }
+            }
+
+            return Math.Min(swapsOne, swapsTwo);
+        }
+    }
+}
