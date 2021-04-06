e6142d5ab832b3769be92e57614e87bd2a07870e
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-05 23:53:11 -0300

    CountBinarySubStrings

---
+++ InterviewPreparation/TrainExercises/Easy/CountBinarySubStrings.cs
@@ -0,0 +1,32 @@
+﻿namespace InterviewPreparation.TrainExercises.Easy
+{
+    class CountBinarySubStrings
+    {
+        public int CountBinarySubstrings(string s)
+        {
+            var lastConsecutiveCount = 1;
+            var consecutiveCount = 1;
+            var total = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] != s[i - 1])
+                {
+                    lastConsecutiveCount = consecutiveCount;
+                    consecutiveCount = 1;
+                    total++;
+                }
+                else
+                {
+                    consecutiveCount++;
+                    if (consecutiveCount <= lastConsecutiveCount)
+                    {
+                        total++;
+                    }
+                }
+            }
+
+            return total;
+        }
+    }
+}
