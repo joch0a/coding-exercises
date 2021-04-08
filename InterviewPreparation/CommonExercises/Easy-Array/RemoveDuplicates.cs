7835033e1f6f61c98ff631a963155f0c6a31f32e
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-07 21:08:00 -0300

    Remove duplicates

---
+++ InterviewPreparation/CommonExercises/Easy-Array/RemoveDuplicates.cs
@@ -0,0 +1,27 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_Array
+{
+    class RemoveDuplicatesFromSortedArray
+
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            var leftPosition = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == 0 || nums[i] != nums[i - 1])
+                {
+                    nums[leftPosition] = nums[i];
+                    leftPosition++;
+                }
+            }
+
+            return leftPosition;
+        }
+    }
+}
