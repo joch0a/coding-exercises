02481f651eed81a4548a50ae3f7f34d2eaef08d7
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-07 21:50:07 -0300

    rotate

---
+++ InterviewPreparation/CommonExercises/Easy-Array/RotateArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterviewPreparation.CommonExercises.Easy_Array
+{
+    class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            int count = 0;
+
+            for (int start = 0; count < nums.Length; start++)
+            {
+                var current = start;
+                var prev = nums[start];
+             
+                do
+                {
+                    var next = (current + k) % nums.Length;
+                    int tmp = nums[next];
+                    nums[next] = prev;
+                    current = next;
+                    prev = tmp;
+                    count++;
+                } while (current != start);
+            }
+        }
+
+        public void RotateWithSpace(int[] nums, int k)
+        {
+            int[] aux = new int[nums.Length];
+            k = k % nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                aux[(i + k) % nums.Length] = nums[i];
+            }
+
+            Array.Copy(aux, nums, aux.Length);
+        }
+    }
+}
