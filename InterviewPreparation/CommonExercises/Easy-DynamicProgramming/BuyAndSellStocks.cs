0e8cb52abc8e5038a7944c872564ebaa49705899
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 19:29:44 -0300

    Buy&Sell stocks

---
+++ InterviewPreparation/CommonExercises/Easy-DynamicProgramming/BuyAndSellStocks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterviewPreparation.CommonExercises.Easy_DynamicProgramming
+{
+    class BuyAndSellStocks
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var profits = new int[prices.Length + 1];
+            var max = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                profits[i] = Math.Max(0, profits[i - 1] + (prices[i] - prices[i - 1]));
+                max = Math.Max(profits[i], max);
+            }
+
+            return max;
+        }
+    }
+}
