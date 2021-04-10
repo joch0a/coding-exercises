b11107035912c6597ce6710a068c09e6fc540b4a
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:03:57 -0300

    Move zeroes

---
+++ InterviewPreparation/CommonExercises/Easy-Array/MoveZeroes.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_Array
+{
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+    class MoveZeroes
+    {
+        public void Move(int[] nums)
+        {
+            var fillIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    if (fillIndex != i)
+                    {
+                        nums[fillIndex] = nums[i];
+                        nums[i] = 0;
+                    }
+
+                    fillIndex++;
+                }
+            }
+        }
+    }
+}
