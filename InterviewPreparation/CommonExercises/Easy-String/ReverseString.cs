e97fd236f1a148cc13f52b7a95719ed484a6e192
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:05:17 -0300

    Reverse string

---
+++ InterviewPreparation/CommonExercises/Easy-String/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_String
+{
+    class ReverseString
+    {
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+        public void Reverse(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            char tmp;
+
+            while (i < j)
+            {
+                tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+
+                j--;
+                i++;
+            }
+        }
+    }
+}
