ffdb4201284b284c8f7a05d31ebe695131e7c537
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:05:00 -0300

    Palindrome

---
+++ InterviewPreparation/CommonExercises/Easy-String/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_String
+{
+    class Palindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                while (i < j && !char.IsDigit(s[i]) && !char.IsLetter(s[i]))
+                {
+                    i++;
+                }
+                while (i < j && !char.IsDigit(s[j]) && !char.IsLetter(s[j]))
+                {
+                    j--;
+                }
+
+                if (i < j && char.ToLower(s[i]) != char.ToLower(s[j]))
+                {
+
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
