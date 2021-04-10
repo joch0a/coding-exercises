bef898c26f3e76afda4b84d4ee2d576c39e5dbe0
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:04:52 -0300

    Longest prefix

---
+++ InterviewPreparation/CommonExercises/Easy-String/LongestPrefix.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace InterviewPreparation.CommonExercises.Easy_String
+{
+    //https://leetcode.com/problems/longest-common-prefix
+    class LongestPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return "";
+            }
+
+            var prefix = new StringBuilder();
+
+            int i = 0;
+            int j = 0;
+            char actualChar = ' ';
+
+            while (true)
+            {
+                if (j >= strs[i].Length)
+                {
+                    return prefix.ToString();
+                }
+
+                if (i == 0)
+                {
+                    actualChar = strs[i][j];
+                }
+                else if (actualChar != strs[i][j])
+                {
+                    return prefix.ToString();
+                }
+
+                i = (i + 1) % strs.Length;
+
+                if (i == 0)
+                {
+                    prefix.Append(actualChar);
+                    j++;
+                }
+            }
+        }
+    }
+}
