045fce862607efc07720821b48d73ecda3d615fc
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:04:11 -0300

    Two Sum

---
+++ InterviewPreparation/CommonExercises/Easy-Array/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.CommonExercises.Easy_Array
+{
+    //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/
+    class TwoSum
+    {
+        public int[] GetTwoSum(int[] nums, int target)
+        {
+            var hs = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hs.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { i, hs[target - nums[i]] };
+                }
+                else
+                {
+                    hs.Add(nums[i], i);
+                }
+            }
+
+            return null;
+        }
+    }
+}
