4a328417c80a7dd49475a27dcde59fb3da8a3733
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:03:32 -0300

    Add one

---
+++ InterviewPreparation/CommonExercises/Easy-Array/AddOne.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace InterviewPreparation.CommonExercises.Easy_Array
+{
+    class AddOne
+    {
+        // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+
+        public int[] PlusOne(int[] digits)
+        {
+            int carry = 1;
+
+            for (int i = digits.Length - 1; i >= 0 && carry != 0; i--)
+            {
+                var actual = digits[i] + carry;
+                carry = actual / 10;
+
+                digits[i] = actual % 10;
+            }
+
+            if (carry != 0)
+            {
+                digits = new int[] { 1 }.Concat(digits).ToArray();
+            }
+
+            return digits;
+        }
+    }
+}
