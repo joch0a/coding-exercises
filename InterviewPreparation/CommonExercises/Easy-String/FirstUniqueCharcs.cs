1d07b85370108d7f7512ca0afccdda4aba28cf3f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:04:43 -0300

    First unique char

---
+++ InterviewPreparation/CommonExercises/Easy-String/FirstUniqueCharcs.cs
@@ -0,0 +1,37 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_String
+{
+    //https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
+    class FirstUniqueCharcs
+    {
+        public int FirstUniqChar(string s)
+        {
+
+            // -1 repeated
+            // 0 not found
+            // > 0 arr[i] - 1 = position
+            var charPositions = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charPositions[s[i] - 'a'] == 0)
+                {
+                    charPositions[s[i] - 'a'] = i + 1;
+                }
+                else
+                {
+                    charPositions[s[i] - 'a'] = -1;
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charPositions[s[i] - 'a'] > 0)
+                {
+                    return charPositions[s[i] - 'a'] - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
