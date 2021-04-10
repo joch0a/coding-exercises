b9a4f369add5c25c4ab4739f4869fcfcd916377d
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 00:05:32 -0300

    ValidAnagram

---
+++ InterviewPreparation/CommonExercises/Easy-String/ValidAnagram.cs
@@ -0,0 +1,31 @@
+﻿namespace InterviewPreparation.CommonExercises.Easy_String
+{
+    class ValidAnagram
+    {
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
+        public bool IsAnagram(string s, string t)
+        {
+            int[] bucket = new int[26];
+
+            foreach (var character in s)
+            {
+                bucket[character - 'a']++;
+            }
+
+            foreach (var character in t)
+            {
+                bucket[character - 'a']--;
+            }
+
+            foreach (var b in bucket)
+            {
+                if (b != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
