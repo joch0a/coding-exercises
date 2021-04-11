114919505d8abd470d051df018bad3fce1001496
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-10 23:31:11 -0300

    Reverse list

---
+++ InterviewPreparation/CommonExercises/Easy-List/ReverseList.cs
@@ -0,0 +1,23 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.CommonExercises.Easy_List
+{
+    class ReverseList
+    {
+        public ListNode Solution(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            else
+            {
+                var actual = Solution(head.next);
+                head.next.next = head;
+                head.next = null;
+
+                return actual;
+            }
+        }
+    }
+}
