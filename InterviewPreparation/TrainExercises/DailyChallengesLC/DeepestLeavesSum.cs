f98b7ceaee7921bebc96a41646ff29f0e55d85fe
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-11 15:36:37 -0300

    Deepest Leaves Sum

---
+++ InterviewPreparation/TrainExercises/DailyChallengesLC/DeepestLeavesSum.cs
@@ -0,0 +1,47 @@
+﻿using InterviewPreparation.Exercises;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.TrainExercises.DailyChallengesLC
+{
+    class DeepestLeavesSum
+    {
+        public int Solve(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+
+            var queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var queueSize = queue.Count;
+                sum = 0;
+
+                while (queueSize > 0)
+                {
+                    var actual = queue.Dequeue();
+                    queueSize--;
+                    sum += actual.val;
+
+                    if (actual.left != null)
+                    {
+                        queue.Enqueue(actual.left);
+                    }
+
+                    if (actual.right != null)
+                    {
+                        queue.Enqueue(actual.right);
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
