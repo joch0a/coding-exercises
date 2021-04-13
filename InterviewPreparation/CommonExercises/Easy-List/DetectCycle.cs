e68c05cb10fc22962a34c2340025c26ae1b2b1c0
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-13 02:10:14 -0300

    detect cycles

---
+++ InterviewPreparation/CommonExercises/Easy-List/DetectCycle.cs
@@ -0,0 +1,31 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.CommonExercises.Easy_List
+{
+    class DetectCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var slow = head;
+            ListNode fast = head.next;
+
+            while (fast != null && fast.next != null)
+            {
+                if (fast == slow)
+                {
+                    return true;
+                }
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return false;
+        }
+    }
+}
