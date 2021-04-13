97fff51052731ed4ac9b104f0e5a67e84583a5d4
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-13 03:16:17 -0300

    RemoveNthFromEnd

---
+++ InterviewPreparation/CommonExercises/Easy-List/RemoveNTHElement.cs
@@ -0,0 +1,31 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.CommonExercises.Easy_List
+{
+    class RemoveNTHElement
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode dummy = new ListNode(0);
+            ListNode first = dummy;
+            ListNode second = dummy;
+
+            dummy.next = head;
+
+            for (int i = 1; i <= n + 1; i++)
+            {
+                first = first.next;
+            }
+            
+            while (first != null)
+            {
+                first = first.next;
+                second = second.next;
+            }
+            
+            second.next = second.next.next;
+            
+            return dummy.next;
+        }
+    }
+}
