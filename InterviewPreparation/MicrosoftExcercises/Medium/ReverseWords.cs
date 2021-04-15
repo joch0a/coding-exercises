92384fcb5d242dacfa9a75ab74f642ed08a94d1c
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-15 01:10:38 -0300

    Reverse words

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/ReverseWords.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class ReverseWords
+    {
+        public string Solve(string s)
+        {
+            var sb = new StringBuilder();
+            int i = 0;
+
+            while (i < s.Length)
+            {
+                while (i < s.Length && s[i] == ' ')
+                {
+                    i++;
+                }
+
+                var actual = new StringBuilder();
+
+                while (i < s.Length && char.IsLetterOrDigit(s[i]))
+                {
+                    actual.Append(s[i]);
+                    i++;
+                }
+                actual.Append(' ');
+
+                sb.Insert(0, actual.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
