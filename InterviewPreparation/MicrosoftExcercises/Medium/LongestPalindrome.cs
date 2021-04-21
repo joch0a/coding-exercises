f9d73a659b69ae095d7004ed6c6b5cfe4b4a7425
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-21 18:39:26 -0300

    Longest palindrome

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/LongestPalindrome.cs
@@ -0,0 +1,28 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class LongestPalindrome
+    {
+        public string Solve(string s)
+        {
+            var maxPalindrome = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                var p1 = CalculatePalindrome(s, i, i);
+                var p2 = CalculatePalindrome(s, i, i + 1);
+
+                maxPalindrome = maxPalindrome.Length > p1.Length ? maxPalindrome : p1;
+                maxPalindrome = maxPalindrome.Length > p2.Length ? maxPalindrome : p2;
+            }
+            return maxPalindrome;
+        }
+        public string CalculatePalindrome(string s, int i, int j)
+        {
+            while (i >= 0 && j < s.Length && s[i] == s[j])
+            {
+                i--;
+                j++;
+            }
+            return s.Substring(i + 1, j - i - 1);
+        }
+    }
+}
