43cfe4c7895585289b7e68733d6133e6c329042f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-21 18:39:18 -0300

    BuySell stocks

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/BestTimeToSellAndBuy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class BestTimeToSellAndBuy
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var profits = new int[prices.Length];
+            var max = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                profits[i] = Math.Max(profits[i - 1] + (prices[i] - prices[i - 1]), 0);
+                max = Math.Max(profits[i], max);
+            }
+            return max;
+        }
+    }
+}
