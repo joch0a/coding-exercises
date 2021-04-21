d008064c4bfec22533adfbde7892580242093198
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-21 18:39:47 -0300

    Validate BST iterative

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/ValidateBST.cs
@@ -0,0 +1,28 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class ValidateBST
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsValidBST(root, null, null);
+        }
+
+        private bool IsValidBST(TreeNode root, int? min, int? max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if ((min != null && root.val <= min) ||
+                (max != null && root.val >= max))
+            {
+                return false;
+            }
+
+            return IsValidBST(root.left, min, root.val) && IsValidBST(root.right, root.val, max);
+        }
+    }
+}
