a83815b9861ae4b3780e03c07421507c153ad9d2
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-22 13:54:40 -0300

    TrimBST

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/TrimBST.cs
@@ -0,0 +1,38 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class TrimBST
+    {
+        public TreeNode Solve(TreeNode root, int low, int high)
+        {
+            return PostOrder(root, low, high);
+        }
+
+        public TreeNode PostOrder(TreeNode root, int low, int high)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            else
+            {
+                root.left = PostOrder(root.left, low, high);
+                root.right = PostOrder(root.right, low, high);
+
+                if (root.val >= low && root.val <= high)
+                {
+                    return root;
+                }
+                else if (root.val < low)
+                {
+                    return root.right;
+                }
+                else
+                {
+                    return root.left;
+                }
+            }
+        }
+    }
+}
