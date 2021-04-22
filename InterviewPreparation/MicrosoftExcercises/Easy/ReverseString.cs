41ebc5406d10ad5ae3f425843a50d481caf74f23
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-22 13:57:55 -0300

    reverse string

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ReverseString
+    {
+        public void Solve(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            char tmp;
+
+            while (i < j)
+            {
+                tmp = s[i];
+                s[i] = s[j];
+                s[j] = tmp;
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
