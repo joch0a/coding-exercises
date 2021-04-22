88a036905a1988782911c34eeafe05d21becca24
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-22 13:54:34 -0300

    LinkedListCycle

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/LinkedListCycle.cs
@@ -0,0 +1,31 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+
+            var fast = head;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
