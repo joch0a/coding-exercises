5fbc775dbdb87f24bd10ad4c51cd0cd8295bd588
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-22 23:48:34 -0300

    Longest substring w/o repeateds

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/LongestSubstringNoRepeat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class LongestSubstringNoRepeat
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var positions = new Dictionary<char, int>();
+            var longest = 0;
+            var i = 0;
+            for (int j = 0; j < s.Length; j++)
+            {
+                var visited = positions.ContainsKey(s[j]);
+
+                if (visited)
+                {
+                    i = Math.Max(i, positions[s[j]]);
+                }
+
+                longest = Math.Max(longest, j - i + 1);
+
+                if (visited)
+                {
+                    positions[s[j]] = j + 1;
+                }
+                else
+                {
+                    positions.Add(s[j], j + 1);
+                }
+            }
+
+            return longest;
+        }
+    }
+}
