238a948e4da23459cdfc9521440e2bb44c345971
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-23 10:09:25 -0300

    2d array

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/Search2DArray.cs
@@ -0,0 +1,31 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class Search2DArray
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var i = matrix.Length - 1;
+            var j = 0;
+
+            while (i >= 0 && j < matrix[i].Length)
+            {
+                var actual = matrix[i][j];
+
+                if (actual == target)
+                {
+                    return true;
+                }
+                else if (actual > target)
+                {
+                    i--;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
