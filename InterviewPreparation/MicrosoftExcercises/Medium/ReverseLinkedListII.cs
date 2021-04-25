3bab01edb2ba5d062a739d357c9d38800cafefa6
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-25 16:15:27 -0300

    reverse linked list II

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/ReverseLinkedListII.cs
@@ -0,0 +1,65 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class ReverseLinkedListII
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (left == right)
+            {
+                return head;
+            }
+
+            var fast = head;
+            var slow = head;
+
+            var diff = right - left;
+
+            for (int i = 0; i <= diff; i++)
+            {
+                fast = fast?.next;
+            }
+
+            ListNode globalPrev = null;
+
+            for (int i = 1; i < left; i++)
+            {
+                globalPrev = slow;
+                fast = fast?.next;
+                slow = slow?.next;
+            }
+
+            ListNode prev = null;
+            ListNode first = null;
+
+            while (slow != fast)
+            {
+                var tmp = slow.next;
+
+                slow.next = prev;
+                prev = slow;
+
+                if (first == null)
+                {
+                    first = slow;
+                }
+
+                slow = tmp;
+            }
+
+            first.next = fast;
+
+            if (globalPrev == null)
+            {
+                return prev;
+            }
+            else
+            {
+                globalPrev.next = prev;
+            }
+
+            return head;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -148,7 +148,7 @@ The following exercises were randomly picked it from LC to test what I've learnt
 # Complete study plan 
 ## The following excercise should be resolved WITHOUT looking to any solution based on previous ones.
 
-## Actual state 38/287  12.54%
+## Actual state 39/287  13.58%
 | Name      | Resource link | Status |
 | --------- | ----- | -----:|
 | Copy List with Random Pointer | https://leetcode.com/problems/copy-list-with-random-pointer | :white_check_mark: :grey_exclamation: |
@@ -208,7 +208,7 @@ The following exercises were randomly picked it from LC to test what I've learnt
 | Serialize and Deserialize N-ary Tree | https://leetcode.com/problems/serialize-and-deserialize-n-ary-tree | :X: |
 | Coin Change | https://leetcode.com/problems/coin-change | :white_check_mark: |
 | Trapping Rain Water | https://leetcode.com/problems/trapping-rain-water | :o: |
-| Reverse Linked List II | https://leetcode.com/problems/reverse-linked-list-ii | :o: |
+| Reverse Linked List II | https://leetcode.com/problems/reverse-linked-list-ii | :white_check_mark: |
 | Intersection of Two Linked Lists | https://leetcode.com/problems/intersection-of-two-linked-lists | :o: |
 | Minesweeper | https://leetcode.com/problems/minesweeper | :o: |
 | Trim a Binary Search Tree | https://leetcode.com/problems/trim-a-binary-search-tree | :white_check_mark: |
