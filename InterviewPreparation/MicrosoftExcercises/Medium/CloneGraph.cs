032de49691743ca0389c4e8f784fac509521751d
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-26 20:08:20 -0300

    Clone graph

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CloneGraph.cs
@@ -0,0 +1,49 @@
+﻿using InterviewPreparation.Shared;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class CloneGraph
+    {
+        public GraphNode Solve(GraphNode node)
+        {
+            if (node == null)
+            {
+                return node;
+            }
+
+            var dict = new Dictionary<GraphNode, GraphNode>();
+            var queue = new Queue<GraphNode>();
+
+            queue.Enqueue(node);
+
+            while (queue.Count > 0)
+            {
+                var actual = queue.Dequeue();
+
+                if (!dict.ContainsKey(actual))
+                {
+                    dict.Add(actual, new GraphNode(actual.val));
+
+                    foreach (var child in actual.neighbors)
+                    {
+                        if (!dict.ContainsKey(child))
+                        {
+                            queue.Enqueue(child);
+                        }
+                    }
+                }
+            }
+
+            foreach (var actualGraphNode in dict.Keys)
+            {
+                foreach (var child in actualGraphNode.neighbors)
+                {
+                    dict[actualGraphNode].neighbors.Add(dict[child]);
+                }
+            }
+
+            return dict[node];
+        }
+    }
+}
---
+++ InterviewPreparation/Shared/GraphNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.Shared
+{
+    public class GraphNode
+    {
+        public int val;
+        public IList<GraphNode> neighbors;
+
+        public GraphNode()
+        {
+            val = 0;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int _val)
+        {
+            val = _val;
+            neighbors = new List<GraphNode>();
+        }
+
+        public GraphNode(int _val, List<GraphNode> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
