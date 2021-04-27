1063225b0625f5875d295a484ae374fb6384e144
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-27 11:27:04 -0300

    first unique char in string

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/FirstUniqueCharInString.cs
@@ -0,0 +1,24 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class FirstUniqueCharInString
+    {
+        public int FirstUniqChar(string s)
+        {
+            var bucket = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bucket[s[i] - 'a']++;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (bucket[s[i] - 'a'] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
