de6526d18d86ec0f40e8a4e106db5aabd355537a
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-27 11:27:14 -0300

    remove duplicates

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/RemoveElements.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class RemoveElements
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+            var leftPosition = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[leftPosition] != nums[i])
+                {
+                    leftPosition++;
+                    nums[leftPosition] = nums[i];
+                }
+            }
+
+            return leftPosition + 1;
+        }
+    }
+}
