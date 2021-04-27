a25b8a5355f1f3931d4a6f19ea4856c5f9e3b062
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-26 21:34:05 -0300

    permutations

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/Permutations.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class Permutations
+    {
+        public HashSet<int> visited { get; set; } = new HashSet<int>();
+
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var result = new List<IList<int>>();
+
+            foreach (var num in nums)
+            {
+                visited.Add(num);
+
+                Backtrack(nums, result);
+
+                visited.Remove(num);
+            }
+
+            return result;
+        }
+
+        public void Backtrack(int[] nums, IList<IList<int>> result)
+        {
+            if (visited.Count == nums.Length)
+            {
+                result.Add(visited.ToList());
+                return;
+            }
+
+            foreach (var num in nums)
+            {
+                if (!visited.Contains(num))
+                {
+                    visited.Add(num);
+
+                    Backtrack(nums, result);
+
+                    visited.Remove(num);
+                }
+            }
+        }
+    }
+}
