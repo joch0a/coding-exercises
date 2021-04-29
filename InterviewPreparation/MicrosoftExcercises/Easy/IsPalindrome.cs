269fe417c2ddcb7c48b779d5fa1b38c84b71fcf6
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-28 21:58:36 -0300

    palindrome

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/IsPalindrome.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class IsPalindrome
+    {
+        public bool Solve(int x)
+        {
+            var str = x.ToString();
+            int i = 0;
+            int j = str.Length - 1;
+
+            while (i <= j)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
