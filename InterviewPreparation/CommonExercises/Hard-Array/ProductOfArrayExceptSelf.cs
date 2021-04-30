b9da556a9546cf8dc6396bf8781c2fc5ff2abb7f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-04-29 22:39:17 -0300

    Product of Array Except Self

---
+++ InterviewPreparation/CommonExercises/Hard-Array/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,27 @@
+﻿namespace InterviewPreparation.CommonExercises.Hard_Array
+{
+    class ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+
+            result[0] = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+
+            var right = 1;
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                right = right * nums[i + 1];
+                result[i] = result[i] * right;
+            }
+
+            return result;
+        }
+    }
+}
