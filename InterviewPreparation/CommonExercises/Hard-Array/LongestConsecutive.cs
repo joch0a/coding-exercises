327a7f55543b7f0aec74d6d478f0742881c65d00
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-02 11:05:17 -0300

    Longest Consecutive

---
+++ InterviewPreparation/CommonExercises/Hard-Array/LongestConsecutive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.CommonExercises.Hard_Array
+{
+    class LongestConsecutive
+    {
+        public int Solve(int[] nums)
+        {
+            var maxLongestStreak = 0;
+
+            var hs = new HashSet<int>(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var actual = nums[i];
+
+                if (!hs.Contains(actual - 1))
+                {
+                    var longestStreak = 0;
+
+                    while (hs.Contains(actual))
+                    {
+                        longestStreak++;
+                        actual++;
+                    }
+
+                    maxLongestStreak = Math.Max(maxLongestStreak, longestStreak);
+                }
+            }
+
+            return maxLongestStreak;
+        }
+    }
+}
