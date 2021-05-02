4fd1d492d8144bbbc378cc90d3591f78ff5c7b33
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-02 15:40:18 -0300

    Sliding Window Maximum

---
+++ InterviewPreparation/CommonExercises/Hard-Array/SlidingWindowMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewPreparation.CommonExercises.Hard_Array
+{
+    class SlidingWindowMaximum
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            var result = new int[nums.Length - k + 1];
+            var queue = new LinkedList<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (queue.Any() && queue.First.Value + k == i)
+                {
+                    queue.RemoveFirst();
+                }
+
+                while (queue.Any() && nums[i] > nums[queue.Last.Value])
+                {
+                    queue.RemoveLast();
+                }
+
+                queue.AddLast(i);
+
+                if (i + 1 - k >= 0)
+                {
+                    result[i + 1 - k] = nums[queue.First.Value];
+                }
+            }
+
+            return result;
+        }
+    }
+}
