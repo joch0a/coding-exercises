cafb7717cf4a21dbefb58ca0b11454003e6980ee
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-02 11:05:09 -0300

    Find Duplicate

---
+++ InterviewPreparation/CommonExercises/Hard-Array/FindDuplicate.cs
@@ -0,0 +1,32 @@
+﻿namespace InterviewPreparation.CommonExercises.Hard_Array
+{
+    class FindDuplicate
+    {
+        public int Solve(int[] nums)
+        {
+            var slow = 0;
+            var fast = 0;
+
+            while (fast < nums.Length && nums[fast] < nums.Length)
+            {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+
+                if (fast == slow)
+                {
+                    break;
+                }
+            }
+
+            slow = 0;
+
+            while (slow != fast)
+            {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+
+            return slow;
+        }
+    }
+}
