dddcc030a39aef67323c2e0c76aff6ff07875989
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-06 21:23:24 -0300

    number of provinces

---
+++ InterviewPreparation/CommonExercises/Hard-Graph/NumberOfProvinces.cs
@@ -0,0 +1,67 @@
+﻿using InterviewPreparation.Shared;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.CommonExercises.Hard_Graph
+{
+    class NumberOfProvinces
+    {
+        //Union-Find solution
+        public int FindCircleNum(int[][] M)
+        {
+
+            int n = M.Length;
+            var uf = new UnionFind(n);
+
+            for (int i = 0; i < M.Length; i++)
+            {
+                for (int j = 0; j < M[0].Length; j++)
+                {
+                    if (M[i][j] == 1 && i != j) // i != j (can't be friend to oneself)
+                        uf.Union(i, j);
+                }
+            }
+            return uf.ConnectedComponents();
+        }
+
+        public int FindCircleNumBFS(int[][] isConnected)
+        {
+            var totalCount = 0;
+            var visited = new HashSet<int>();
+
+            for (int i = 0; i < isConnected.Length; i++)
+            {
+                if (!visited.Contains(i))
+                {
+                    totalCount++;
+                    BFS(i, isConnected, visited);
+                }
+            }
+
+            return totalCount;
+        }
+
+        public void BFS(int i, int[][] isConnected, HashSet<int> visited)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(i);
+
+            while (queue.Count > 0)
+            {
+                var actual = queue.Dequeue();
+
+                if (!visited.Contains(actual))
+                {
+                    visited.Add(actual);
+
+                    for (int j = 0; j < isConnected[actual].Length; j++)
+                    {
+                        if (actual != j && !visited.Contains(j) && isConnected[actual][j] == 1)
+                        {
+                            queue.Enqueue(j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ InterviewPreparation/Shared/UnionFind.cs
@@ -0,0 +1,67 @@
+﻿namespace InterviewPreparation.Shared
+{
+    public class UnionFind
+    {
+        private int[] id;
+        private int[] weights;
+
+        public UnionFind(int n)
+        {
+            id = new int[n];
+            weights = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        public void Union(int p, int q)
+        {
+            int i = Find(p);
+            int j = Find(q);
+
+            if (weights[i] < weights[j])
+            {
+                id[i] = j;
+                weights[j] += weights[i];
+            }
+            else
+            {
+                id[j] = i;
+                weights[i] += weights[j];
+            }
+
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Find(p) == Find(q);
+        }
+
+        public int Find(int i) //path compression
+        {
+            while (i != id[i])
+            {
+                //id[i] = id[id[i]]; one pass compression
+                i = id[i];
+            }
+            while (i != id[i]) // 2 pass compression ( more efficient)
+            {
+                id[i] = i;
+            }
+            return i;
+        }
+
+        public int ConnectedComponents()
+        {
+            int count = 0;
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (id[i] == i)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
