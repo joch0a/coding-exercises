0e1dfc0cfa05bd0a6ad5ed4bf029ebb0905db3be
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:24:08 -0300

    Longest prefix

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/LongestPrefix.cs
@@ -0,0 +1,28 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class LongestPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+
+            for (int j = 0; j < strs[0].Length; j++)
+            {
+                var charAt = strs[0][j];
+
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    if (j == strs[i].Length || charAt != strs[i][j])
+                    {
+                        return strs[0].Substring(0, j);
+                    }
+                }
+            }
+
+            return strs[0];
+        }
+    }
+}
