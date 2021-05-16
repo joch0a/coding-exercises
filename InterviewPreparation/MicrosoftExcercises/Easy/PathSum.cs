42033d4e15a63acea396af63b6396389330de1d2
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:24:20 -0300

    Path sum

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/PathSum.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class PathSum
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (sum - root.val == 0 && root.left == null && root.right == null)
+            {
+                return true;
+            }
+
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+    }
+}
