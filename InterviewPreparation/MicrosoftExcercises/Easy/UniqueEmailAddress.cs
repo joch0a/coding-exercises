f320a3e36eb0f2b5783b251268e0c128aa76d30f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:24:39 -0300

    Unique Email Addresses

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/UniqueEmailAddress.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class UniqueEmailAddress
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var finalEmails = new HashSet<string>();
+
+            foreach (var email in emails)
+            {
+                var parseEmail = new StringBuilder();
+                var atSignfound = false;
+                var plusSignFound = false;
+
+                for (var i = 0; i < email.Length; i++)
+                {
+                    var actual = email[i];
+
+                    atSignfound |= actual == '@';
+                    plusSignFound |= actual == '+';
+
+                    if (atSignfound)
+                    {
+                        parseEmail.Append(actual);
+                    }
+                    else if (!plusSignFound && actual != '.')
+                    {
+                        parseEmail.Append(actual);
+                    }
+                }
+
+                finalEmails.Add(parseEmail.ToString());
+
+            }
+
+            return finalEmails.Count;
+        }
+    }
+}
