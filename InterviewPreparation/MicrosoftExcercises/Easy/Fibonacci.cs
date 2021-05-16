59b1348d6f7f87211f021c19c9cd3f78e5a391bd
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:23:55 -0300

    Fibo

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class Fibonacci
+    {
+        public int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            var f1 = 0;
+            var f2 = 1;
+            int next;
+
+            for (int i = 1; i < n; i++)
+            {
+                next = f1 + f2;
+                f1 = f2;
+                f2 = next;
+            }
+
+            return f2;
+        }
+    }
+}
