40a89a34de80bdf4f0ceb858f5d35430d33987cb
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:24:02 -0300

    Intersection two lists

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/IntersectionOfTwoList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class IntersectionOfTwoList
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length > nums2.Length)
+            {
+                var aux = nums1;
+                nums1 = nums2;
+                nums2 = aux;
+            }
+
+            var hsNums1 = nums1.ToHashSet();
+            var solution = new HashSet<int>();
+
+            foreach (var num in nums2)
+            {
+                if (hsNums1.Contains(num))
+                {
+                    solution.Add(num);
+                }
+            }
+
+            return solution.ToArray();
+        }
+    }
+}
