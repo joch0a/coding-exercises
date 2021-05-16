5d84e05216029c5b4b57cf33ee5ec3f6f80cecbc
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-16 12:24:14 -0300

    Min depth

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/MinDepth.cs
@@ -0,0 +1,52 @@
+﻿using InterviewPreparation.Exercises;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class MinDepth
+    {
+        public int Solve(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var queue = new Queue<TreeNode>();
+            var level = 0;
+
+            queue.Enqueue(root);
+
+            while (queue.Any())
+            {
+                var queueSize = queue.Count;
+                level++;
+
+                while (queueSize > 0)
+                {
+                    var actual = queue.Dequeue();
+
+                    if (actual.left == null && actual.right == null)
+                    {
+                        return level;
+                    }
+
+                    if (actual.left != null)
+                    {
+                        queue.Enqueue(actual.left);
+                    }
+
+                    if (actual.right != null)
+                    {
+                        queue.Enqueue(actual.right);
+                    }
+
+                    queueSize--;
+                }
+            }
+
+            return level;
+        }
+    }
+}
