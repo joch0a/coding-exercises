c63da19aa0bee3cd9ba60de293a4bcf8436c0e54
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-17 19:24:54 -0300

    Delete duplicates

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/DeleteDuplicates.cs
@@ -0,0 +1,24 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class DeleteDuplicates
+    {
+        public ListNode Solve(ListNode head)
+        {
+            var actual = head;
+            while (actual != null && actual.next != null)
+            {
+                if (actual.val == actual.next.val)
+                {
+                    actual.next = actual.next.next;
+                }
+                else
+                {
+                    actual = actual.next;
+                }
+            }
+            return head;
+        }
+    }
+}
