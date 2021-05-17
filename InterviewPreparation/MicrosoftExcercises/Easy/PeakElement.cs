78b2e2edb5268b97c25a5c5e94da720e3e6c04ed
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-17 19:25:07 -0300

    Peak element

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/PeakElement.cs
@@ -0,0 +1,29 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class PeakElement
+    {
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            var low = 0;
+            var high = arr.Length - 1;
+            while (low < high)
+            {
+                var mid = low + (high - low) / 2;
+
+                if (mid == 0 || mid == arr.Length - 1 || (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]))
+                {
+                    return mid;
+                }
+                else if (arr[mid] > arr[mid + 1] && arr[mid] < arr[mid - 1])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
