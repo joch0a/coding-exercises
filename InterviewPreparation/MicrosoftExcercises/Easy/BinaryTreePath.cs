39eb73315ef9e10f494f2c8bd90f17708f21d2ea
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-17 19:24:47 -0300

    Binary tree path

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/BinaryTreePath.cs
@@ -0,0 +1,38 @@
+﻿using InterviewPreparation.Exercises;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class BinaryTreePath
+    {
+        public IList<string> result;
+
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            result = new List<string>();
+            BinaryTreePathsAux(root, "");
+            return result;
+        }
+
+        private void BinaryTreePathsAux(TreeNode root, string path)
+        {
+            if (root != null)
+            {
+                if (root.left == null && root.right == null)
+                {
+                    path += $"{root.val}";
+                    result.Add(path);
+                }
+                path += $"{root.val}->";
+                if (root.left != null)
+                {
+                    BinaryTreePathsAux(root.left, path);
+                }
+                if (root.right != null)
+                {
+                    BinaryTreePathsAux(root.right, path);
+                }
+            }
+        }
+    }
+}
