07dec2103e31960e513a1a2178a712cac9d914c5
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-18 20:44:53 -0300

    WordPattern

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/WordPattern.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class WordPattern
+    {
+        public bool Solve(string pattern, string s)
+        {
+            var dictPattern = new Dictionary<char, string>();
+            var dictWords = new Dictionary<string, char>();
+            var words = s.Split(' ');
+
+            if (words.Length != pattern.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!dictPattern.ContainsKey(pattern[i]) && !dictWords.ContainsKey(words[i]))
+                {
+                    dictPattern.Add(pattern[i], words[i]);
+                    dictWords.Add(words[i], pattern[i]);
+                }
+                else if (dictPattern.ContainsKey(pattern[i]) && dictWords.ContainsKey(words[i]))
+                {
+                    if (dictPattern[pattern[i]] != words[i] || dictWords[words[i]] != pattern[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
