fd6660f48abcfdbab9f5eb2c9a840f167880b1ff
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-21 20:28:19 -0300

    Buy stocks

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/BuyStock.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class BuyStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            var profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                var diff = prices[i] - prices[i - 1];
+                if (diff > 0)
+                {
+                    profit += diff;
+                }
+            }
+
+            return profit;
+        }
+    }
+}
