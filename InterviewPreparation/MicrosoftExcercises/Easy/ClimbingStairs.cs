8a184fccc75b8a01565f4a55328846ff6d1dd5a1
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:42:27 -0300

    ClimbingStairs

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ClimbingStairs.cs
@@ -0,0 +1,25 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ClimbingStairs
+    {
+        public int Solve(int n)
+        {
+            if (n < 3)
+            {
+                return n;
+            }
+
+            var dp = new int[n];
+
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
