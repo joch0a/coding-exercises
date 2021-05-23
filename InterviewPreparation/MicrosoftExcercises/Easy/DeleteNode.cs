e69d16924a79971c81a84fa3a532e2ac68ed304b
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 19:59:37 -0300

    Delete Node

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/DeleteNode.cs
@@ -0,0 +1,23 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class DeleteNode
+    {
+        public void Solve(ListNode node)
+        {
+            var actual = node;
+            ListNode prev = null;
+
+            while (actual != null && actual.next != null)
+            {
+                actual.val = actual.next.val;
+
+                prev = actual;
+                actual = actual.next;
+            }
+
+            prev.next = null;
+        }
+    }
+}
