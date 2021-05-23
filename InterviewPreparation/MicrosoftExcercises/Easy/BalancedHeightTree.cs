823187bd4528fc2a32d2429811445eb7f6d1217d
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:42:19 -0300

    BalancedHeightTree

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/BalancedHeightTree.cs
@@ -0,0 +1,44 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class BalancedHeightTree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            return IsBalancedAux(root) != -1;
+        }
+
+        public int IsBalancedAux(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                var left = IsBalancedAux(root.left);
+
+                if (left == -1)
+                {
+                    return -1;
+                }
+
+                var right = IsBalancedAux(root.right);
+
+                if (right == -1)
+                {
+                    return -1;
+                }
+
+                if (Math.Abs(left - right) > 1)
+                {
+                    return -1;
+                }
+
+                return Math.Max(left, right) + 1;
+            }
+        }
+    }
+}
