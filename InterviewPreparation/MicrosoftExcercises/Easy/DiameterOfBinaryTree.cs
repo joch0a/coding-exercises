975871485951fed6c20d6e1ce014df8ee9336e13
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:42:33 -0300

    DiameterOfBinaryTree

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/DiameterOfBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class DiameterOfBinaryTree
+    {
+        private int diameter;
+
+        public int Solve(TreeNode root)
+        {
+            diameter = 0;
+
+            DiameterOfBinaryTreeAux(root);
+
+            return diameter;
+        }
+
+        public int DiameterOfBinaryTreeAux(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                var left = DiameterOfBinaryTreeAux(root.left);
+                var right = DiameterOfBinaryTreeAux(root.right);
+
+                diameter = Math.Max(diameter, left + right);
+
+                return Math.Max(left, right) + 1;
+            }
+        }
+    }
+}
