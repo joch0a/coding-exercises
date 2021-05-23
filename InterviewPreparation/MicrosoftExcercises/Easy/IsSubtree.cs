6b10e36e1528e7001835eed1840a0729c1c994ea
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:42:42 -0300

    Subtree

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/IsSubtree.cs
@@ -0,0 +1,25 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class IsSubtree
+    {
+        public bool Solve(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null) return true;
+
+            if (s == null || t == null) return false;
+
+            return Traverse(s, t) || Solve(s.left, t) || Solve(s.right, t);
+        }
+
+        public bool Traverse(TreeNode s, TreeNode t)
+        {
+            if (s == null && t == null) return true;
+
+            if (s == null || t == null) return false;
+
+            return s.val == t.val && Traverse(s.left, t.left) && Traverse(s.right, t.right);
+        }
+    }
+}
