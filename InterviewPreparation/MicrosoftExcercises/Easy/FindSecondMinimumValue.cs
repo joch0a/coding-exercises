c94deac799d54aedc795341afe592e8641126832
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 19:59:52 -0300

    Find second minimun value

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/FindSecondMinimumValue.cs
@@ -0,0 +1,42 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class FindSecondMinimumValue
+    {
+        public int Solve(TreeNode root)
+        {
+            var min1 = long.MaxValue;
+            var result = long.MaxValue;
+
+            var stack = new Stack<TreeNode>();
+
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var actual = stack.Pop();
+
+                if (actual.val < min1)
+                {
+                    result = min1;
+                    min1 = actual.val;
+                }
+                else if (actual.val != min1)
+                {
+                    result = Math.Min(actual.val, result);
+                }
+
+                if (actual.left != null)
+                {
+                    stack.Push(actual.left);
+                    stack.Push(actual.right);
+                }
+            }
+
+            return result == long.MaxValue ? -1 : (int)result;
+        }
+    }
+}
