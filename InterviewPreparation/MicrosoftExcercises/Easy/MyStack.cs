7a30a1df00220a2dd98b97f8effe5a57d25e9ba0
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:42:49 -0300

    MyStack

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    public class MyStack
+    {
+        private Queue<int> stack;
+        private Queue<int> queue;
+
+        /** Initialize your data structure here. */
+        public MyStack()
+        {
+            queue = new Queue<int>();
+            stack = new Queue<int>();
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            queue.Enqueue(x);
+
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Dequeue());
+            }
+
+
+            while (queue.Count > 0)
+            {
+                stack.Enqueue(queue.Dequeue());
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            return stack.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            return stack.Peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return stack.Count == 0;
+        }
+    }
+}
