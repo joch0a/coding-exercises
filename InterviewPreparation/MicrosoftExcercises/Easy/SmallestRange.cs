b9c67e8914eccd05476c0c002f5b725841e359de
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:43:09 -0300

    smallestRange

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/SmallestRange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class SmallestRange
+    {
+        public int SmallestRangeI(int[] A, int k)
+        {
+            var min = A[0];
+            var max = A[0];
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                min = Math.Min(min, A[i]);
+                max = Math.Max(max, A[i]);
+            }
+
+            return Math.Max(0, (max - k) - (min + k));
+        }
+    }
+}
