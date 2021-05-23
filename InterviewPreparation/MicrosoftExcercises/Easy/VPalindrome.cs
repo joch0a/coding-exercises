8170a995423f15e4be99363c409a2f238d3e6440
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-23 17:43:21 -0300

    valid palindrome

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/VPalindrome.cs
@@ -0,0 +1,33 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class VPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            var i = 0;
+            var j = s.Length - 1;
+
+            while (i < j)
+            {
+                while (!char.IsLetterOrDigit(s[i]) && i < j)
+                {
+                    i++;
+                }
+
+                while (!char.IsLetterOrDigit(s[j]) && i < j)
+                {
+                    j--;
+                }
+
+                if (char.ToLower(s[i]) != char.ToLower(s[j]))
+                {
+                    return false;
+                }
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
