ca512c94248f6470b665d95c93013c1c729ed7d4
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:52:18 -0300

    Reverse integer

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ReverseInteger.cs
@@ -0,0 +1,34 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var lastPositiveDigit = int.MaxValue % 10;
+            var lastNegativeDigit = int.MinValue % 10;
+
+            var rev = 0;
+
+            while (x != 0)
+            {
+                var digit = x % 10;
+
+                if (rev > int.MaxValue / 10 || (rev == int.MaxValue && digit > lastPositiveDigit))
+                {
+                    return 0;
+                }
+
+                if (rev < int.MinValue / 10 || (rev == int.MinValue && digit > lastNegativeDigit))
+                {
+                    return 0;
+                }
+
+                rev = rev * 10 + digit;
+
+                x /= 10;
+            }
+
+            return rev;
+        }
+    }
+}
