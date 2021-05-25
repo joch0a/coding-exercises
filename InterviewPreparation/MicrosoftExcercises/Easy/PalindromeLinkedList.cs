d3972d23c243ea6fe01e2dd70ff2e16479fde296
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:52:10 -0300

    Palindrome Linked List

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/PalindromeLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class PalindromeLinkedList
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null)
+            {
+                return true;
+            }
+
+            var fast = head;
+            var slow = head;
+            ListNode prev = null;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+
+                var tmp = slow.next;
+                slow.next = prev;
+                prev = slow;
+
+                slow = tmp;
+            }
+
+            if (fast != null && fast.next == null)
+            {
+                slow = slow.next;
+            }
+
+            while (slow != null)
+            {
+                if (prev.val != slow.val)
+                {
+                    return false;
+                }
+
+                slow = slow.next;
+                prev = prev.next;
+            }
+
+            return true;
+        }
+    }
+}
