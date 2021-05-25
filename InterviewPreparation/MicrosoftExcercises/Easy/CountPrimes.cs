087678ef0b7c6e218e5bef644eabf749ef8d1235
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:51:24 -0300

    Count primes

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/CountPrimes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class CountPrimes
+    {
+        public int Solve(int n)
+        {
+            var isPrime = new bool[n];
+
+            var count = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            for (int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j < n; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                if (isPrime[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
