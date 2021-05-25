c13862b5f172edbbb45ea325a6296cb5f508bfcd
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:51:32 -0300

    Title to number

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ExcelTitleToNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ExcelTitleToNumber
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            var result = 0;
+
+            for (int i = 0; i < columnTitle.Length; i++)
+            {
+                result = result * 26 + (columnTitle[i] - 'A' + 1);
+            }
+
+            return result;
+        }
+    }
+}
