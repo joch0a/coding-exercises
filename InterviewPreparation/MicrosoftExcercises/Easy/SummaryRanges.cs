d5ffb71355651ff48ef9ff2bc56ba20e0a8a8a71
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:52:32 -0300

    Summary ranges

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/SummaryRanges.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class SummaryRanges
+    {
+        public IList<string> Solve(int[] nums)
+        {
+            var result = new List<string>();
+            int i = 0;
+            int? prev;
+
+            while (i < nums.Length)
+            {
+                var start = nums[i];
+                prev = null;
+
+                while (i < nums.Length && (prev == null || (nums[i] - prev == 1)))
+                {
+                    prev = nums[i];
+                    i++;
+                }
+
+                var range = start == prev ? $"{start}" : $"{start}->{prev}";
+
+                result.Add(range);
+            }
+
+            return result;
+        }
+    }
+}
