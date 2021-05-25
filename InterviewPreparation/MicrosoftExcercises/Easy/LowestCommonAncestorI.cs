ee1f76ac9ca2e0e78cb67d156dcd8f6c7169e2aa
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:51:46 -0300

    LCA

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/LowestCommonAncestorI.cs
@@ -0,0 +1,31 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class LowestCommonAncestorI
+    {
+        public TreeNode Solve(TreeNode root, TreeNode p, TreeNode q)
+        {
+            var actual = root;
+
+            while (actual != null)
+            {
+                if ((actual.val >= p.val && actual.val <= q.val) ||
+                  (actual.val <= p.val && actual.val >= q.val))
+                {
+                    return actual;
+                }
+                else if (actual.val > p.val && actual.val > q.val)
+                {
+                    actual = actual.left;
+                }
+                else
+                {
+                    actual = actual.right;
+                }
+            }
+
+            return actual;
+        }
+    }
+}
