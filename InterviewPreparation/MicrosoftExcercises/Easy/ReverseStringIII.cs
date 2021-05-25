e0e8c8e45743f1b3ebec5d6d6e549a0180d43f87
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:52:26 -0300

    Reverse string III

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ReverseStringIII.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ReverseStringIII
+    {
+        public string SolveFirstApproach(string s)
+        {
+            var split = s.Split(" ");
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                split[i] = new string(split[i].Reverse().ToArray());
+            }
+
+            return string.Join(" ", split);
+        }
+
+
+        //second approach
+        public string ReverseWords(string s)
+        {
+            var result = new StringBuilder();
+
+            int i = 0;
+
+            while (i <= s.Length)
+            {
+                var start = i;
+
+                while (i != s.Length && s[i] != ' ')
+                {
+                    i++;
+                }
+
+                result.Append(Reverse(s, start, i - 1));
+
+                i++;
+            }
+
+            return result.ToString();
+        }
+
+        public string Reverse(string s, int start, int end)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = end; i >= start; i--)
+            {
+                sb.Append(s[i]);
+            }
+
+            if (end + 1 != s.Length)
+            {
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
