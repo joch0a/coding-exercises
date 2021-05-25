5ef9ef3c68036eb9f414f31bed667d57b95c75e7
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 22:31:54 -0300

    reverse only letters

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/ReverseOnlyLetters.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class ReverseOnlyLetters
+    {
+        public string Solve(string s)
+        {
+            var next = 0;
+            var right = s.Length - 1;
+            var sb = new StringBuilder();
+
+            for (int i = right; i >= 0; i--)
+            {
+                while (next < s.Length && !char.IsLetter(s[next]))
+                {
+                    sb.Append(s[next]);
+                    next++;
+                }
+
+                if (char.IsLetter(s[i]))
+                {
+                    sb.Append(s[i]);
+
+                    next++;
+                }
+            }
+
+            while (next < s.Length && !char.IsLetter(s[next]))
+            {
+                sb.Append(s[next]);
+                next++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
