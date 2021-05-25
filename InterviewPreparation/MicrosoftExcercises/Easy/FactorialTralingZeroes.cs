da1ef9830a34bca387c40909bbfdfe03e2d9e34c
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-24 21:51:40 -0300

    Factorial trailing zeros

---
+++ InterviewPreparation/MicrosoftExcercises/Easy/FactorialTralingZeroes.cs
@@ -0,0 +1,17 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Easy
+{
+    class FactorialTralingZeroes
+    {
+        public int TrailingZeroes(int n)
+        {
+            var total = 0;
+
+            while (n > 0)
+            {
+                total = n / 5 + total;
+                n = n / 5;
+            }
+
+            return total;
+        }
+    }
