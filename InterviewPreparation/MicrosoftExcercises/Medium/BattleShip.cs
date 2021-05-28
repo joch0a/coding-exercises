aad9aeb5d1d44ff43ddb05b15b2cb6d8f61dacbc
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-28 20:57:45 -0300

    count battleships

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/BattleShip.cs
@@ -0,0 +1,24 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class BattleShip
+    {
+        public int CountBattleships(char[][] board)
+        {
+            var count = 0;
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (board[i][j] == 'X' &&
+                       ((i == 0 && j == 0) || (i == 0 && j > 0 && board[i][j - 1] != 'X') ||
+                        (j == 0 && i > 0 && board[i - 1][j] != 'X') ||
+                        (i > 0 && j > 0 && board[i][j - 1] != 'X' && board[i - 1][j] != 'X')))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
