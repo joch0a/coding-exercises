dc2671c9129521d377437bd08e916d084d4c08e9
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-27 21:36:32 -0300

    Connect right

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/ConnectRightcs.cs
@@ -0,0 +1,90 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class ConnectRightcs
+    {
+        //SPACE O(1) solution
+        public Node Connect(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            var actual = root;
+
+            while (actual.left != null)
+            {
+                var nextLevel = actual.left;
+
+                while (actual != null)
+                {
+                    actual.left.next = actual.right;
+
+                    if (actual.next == null)
+                    {
+                        actual.right.next = null;
+                    }
+                    else
+                    {
+                        actual.right.next = actual.next.left;
+                    }
+
+                    actual = actual.next;
+                }
+
+                actual = nextLevel;
+            }
+
+            return root;
+        }
+
+        //BFS QUEUE Approach
+        public Node Connect2(Node root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            var queue = new Queue<Node>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var queueSize = queue.Count;
+                Node prev = null;
+
+                while (queueSize > 0)
+                {
+                    var actual = queue.Dequeue();
+
+                    if (prev != null)
+                    {
+                        prev.next = actual;
+                    }
+
+                    prev = actual;
+
+                    if (actual.left != null)
+                    {
+                        queue.Enqueue(actual.left);
+                    }
+
+                    if (actual.right != null)
+                    {
+                        queue.Enqueue(actual.right);
+                    }
+
+                    queueSize--;
+                }
+            }
+
+            return root;
+        }
+    }
+}
