4907a18804d581969a84a37ff85c0e41004a7e9c
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-28 20:58:00 -0300

    RotateList

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/RotateList.cs
@@ -0,0 +1,48 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class RotateList
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var fast = head;
+            var length = 1;
+
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                length++;
+            }
+
+            k = k % length;
+
+            if (k == 0)
+            {
+                return head;
+            }
+
+            fast.next = head;
+            ListNode prev = null;
+            var newHead = head;
+            var i = 1;
+            var lastIndex = length - k + 1;
+
+            while (i < lastIndex)
+            {
+                prev = newHead;
+                newHead = newHead.next;
+                i++;
+            }
+
+            prev.next = null;
+
+            return newHead;
+        }
+    }
+}
