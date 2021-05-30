bbaca416741a4721d90344e99b614592a62d57bd
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-29 21:30:27 -0300

    MergeIntervals

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/MergeIntervals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            var result = new List<int[]>();
+
+            Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
+
+            int i = 0;
+
+            while (i < intervals.Length)
+            {
+                var start = intervals[i][0];
+                var end = intervals[i][1];
+                var changed = false;
+
+                while (i < intervals.Length && intervals[i][0] <= end)
+                {
+                    end = Math.Max(end, intervals[i][1]);
+                    i++;
+                    changed = true;
+                }
+
+                result.Add(new int[] { start, end });
+
+                if (!changed)
+                {
+                    i++;
+                }
+            }
+
+
+            return result.ToArray();
+        }
+    }
+}
