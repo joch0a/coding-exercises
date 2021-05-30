a5cab788fe1a8b0ce2dcdad3871ddd504def078e
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-30 19:41:21 -0300

    Delete Node in BST

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/DeleteNodeInBST.cs
@@ -0,0 +1,49 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class DeleteNodeInBST
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root.val > key)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else if (root.val < key)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            else //found it
+            {
+                if (root.left == null)
+                {
+                    return root.right;
+                }
+
+                if (root.right == null)
+                {
+                    return root.left;
+                }
+
+                var min = root.right;
+
+                while (min.left != null)
+                {
+                    min = min.left;
+                }
+
+                root.val = min.val;
+
+                root.right = DeleteNode(root.right, min.val);
+            }
+
+            return root;
+        }
+    }
+}
