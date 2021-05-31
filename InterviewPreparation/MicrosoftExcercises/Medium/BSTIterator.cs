2a82fb88098b855d2046e1d9224d1432e30085e3
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-31 19:41:01 -0300

    BST Iterator

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/BSTIterator.cs
@@ -0,0 +1,47 @@
+﻿using InterviewPreparation.Exercises;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    public class BSTIterator
+    {
+
+        private int Index;
+        private IList<TreeNode> Iterator;
+
+        public BSTIterator(TreeNode root)
+        {
+            Index = 0;
+            Iterator = new List<TreeNode>();
+
+            var actual = root;
+            var stack = new Stack<TreeNode>();
+
+            while (actual != null || stack.Count > 0)
+            {
+                while (actual != null)
+                {
+                    stack.Push(actual);
+                    actual = actual.left;
+                }
+
+                actual = stack.Pop();
+
+                Iterator.Add(actual);
+
+                actual = actual.right;
+            }
+        }
+
+        public int Next()
+        {
+            return Iterator.ElementAt(Index++).val;
+        }
+
+        public bool HasNext()
+        {
+            return Index < Iterator.Count;
+        }
+    }
+}
