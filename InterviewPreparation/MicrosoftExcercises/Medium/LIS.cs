fade51ac3dbc01feab5312a8397b414429539751
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-05-31 19:41:06 -0300

    LIS

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/LIS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class LIS
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            var max = 1;
+            var LIS = new int[nums.Length];
+
+            Array.Fill(LIS, 1);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        LIS[i] = Math.Max(LIS[i], LIS[j] + 1);
+                        max = Math.Max(LIS[i], max);
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
