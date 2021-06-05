e501f3720c003f1fee12efe70fa2ed0417dbeb03
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-04 21:14:48 -0300

    CoinChange2

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CoinChangeII.cs
@@ -0,0 +1,22 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class CoinChangeII
+    {
+        public int Change(int amount, int[] coins)
+        {
+            int[] dp = new int[amount + 1];
+
+            dp[0] = 1;
+
+            foreach (var coin in coins)
+            {
+                for (int i = coin; i < dp.Length; i++)
+                {
+                    dp[i] += dp[i - coin];
+                }
+            }
+
+            return dp[amount];
+        }
+    }
+}
