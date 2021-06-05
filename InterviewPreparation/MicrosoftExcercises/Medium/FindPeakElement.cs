d3191c1dcd06b9488dc51da0c044e823cac2e462
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-05 18:05:02 -0300

    Find peak

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/FindPeakElement.cs
@@ -0,0 +1,27 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class FindPeakElement
+    {
+        public int Solve(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+
+                if (nums[mid] > nums[mid + 1])
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
