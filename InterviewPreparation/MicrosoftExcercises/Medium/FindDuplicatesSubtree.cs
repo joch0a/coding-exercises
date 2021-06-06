36627e83ce9fe5765944f3b2c145944cebabd683
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-06 18:51:42 -0300

    Duplicates subtree

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/FindDuplicatesSubtree.cs
@@ -0,0 +1,51 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class FindDuplicatesSubtree
+    {
+        public IList<TreeNode> duplicates = new List<TreeNode>();
+        public Dictionary<string, int> trees = new Dictionary<string, int>();
+
+        public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<TreeNode>();
+            }
+
+            Traverse(root);
+
+            return duplicates;
+        }
+
+        public string Traverse(TreeNode root)
+        {
+            if (root == null)
+            {
+                return "#";
+            }
+
+            var subTree = $"{root.val} {Traverse(root.left)} {Traverse(root.right)}";
+
+            if (trees.ContainsKey(subTree))
+            {
+                if (trees[subTree] == 1)
+                {
+                    duplicates.Add(root);
+                }
+            }
+            else
+            {
+                trees.Add(subTree, 0);
+            }
+
+            trees[subTree] += 1;
+
+            return subTree;
+        }
+    }
+}
