4b5b6ce2272392bae9d6a8c862074489311f9575
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-19 19:56:11 -0300

    partition list

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/PartitionList.cs
@@ -0,0 +1,39 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class PartitionList
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            var dummyLesser = new ListNode();
+            var dummyGreaterOrEqual = new ListNode();
+
+            var currentLesser = dummyLesser;
+            var currentGreaterOrEqual = dummyGreaterOrEqual;
+
+            var current = head;
+
+            while (current != null)
+            {
+                if (current.val < x)
+                {
+                    currentLesser.next = current;
+                    currentLesser = currentLesser.next;
+                }
+                else
+                {
+                    currentGreaterOrEqual.next = current;
+                    currentGreaterOrEqual = currentGreaterOrEqual.next;
+                }
+
+                current = current.next;
+            }
+            currentGreaterOrEqual.next = null;
+
+            currentLesser.next = dummyGreaterOrEqual.next;
+
+            return dummyLesser.next;
+        }
+    }
+}
