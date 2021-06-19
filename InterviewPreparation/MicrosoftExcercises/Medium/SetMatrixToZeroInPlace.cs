0e6720c4cb0a79c18b005ec1bb7e4dd018a508c5
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-19 19:56:20 -0300

    set marix to zero

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/SetMatrixToZeroInPlace.cs
@@ -0,0 +1,54 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class SetMatrixToZeroInPlace
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            var firstColumn = false;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    firstColumn = true;
+                }
+
+                for (int j = 1; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                for (int j = 1; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            if (matrix[0][0] == 0)
+            {
+                for (int j = 1; j < matrix[0].Length; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            if (firstColumn)
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+    }
+}
