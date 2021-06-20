efb4b5540436ba49fa28ad55fce9efa87db36452
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-20 19:28:00 -0300

    Binary tree right side view

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/BinaryTreeRightSideView.cs
@@ -0,0 +1,51 @@
+﻿using InterviewPreparation.Exercises;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class BinaryTreeRightSideView
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var rightSideView = new List<int>();
+
+            if (root == null)
+            {
+                return rightSideView;
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var queueSize = queue.Count;
+                var first = true;
+                while (queueSize > 0)
+                {
+                    if (first)
+                    {
+                        rightSideView.Add(queue.Peek().val);
+                        first = false;
+                    }
+
+                    var current = queue.Dequeue();
+
+                    if (current.right != null)
+                    {
+                        queue.Enqueue(current.right);
+                    }
+
+                    if (current.left != null)
+                    {
+                        queue.Enqueue(current.left);
+                    }
+
+                    queueSize--;
+                }
+            }
+
+            return rightSideView;
+        }
+    }
+}
