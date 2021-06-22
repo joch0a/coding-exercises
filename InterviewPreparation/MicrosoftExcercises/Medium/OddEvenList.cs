b7c11f8ad3acb956bb8a097bbfbb8a23e293115a
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-22 19:43:01 -0300

    Odd Even list

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/OddEvenList.cs
@@ -0,0 +1,41 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class OddEvenList
+    {
+        public ListNode Solve(ListNode head)
+        {
+            var odds = new ListNode();
+            var evens = new ListNode();
+
+            var actualOdd = odds;
+            var actualEven = evens;
+
+            var current = head;
+            var isOdd = true;
+
+            while (current != null)
+            {
+                if (isOdd)
+                {
+                    actualOdd.next = current;
+                    actualOdd = actualOdd.next;
+                }
+                else
+                {
+                    actualEven.next = current;
+                    actualEven = actualEven.next;
+                }
+
+                isOdd = !isOdd;
+                current = current.next;
+            }
+
+            actualOdd.next = evens.next;
+            actualEven.next = null;
+
+            return odds.next;
+        }
+    }
+}
