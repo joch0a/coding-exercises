a44e1db2ef2da6cff86bee683245848109afc1fe
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-23 19:54:59 -0300

    CanJump

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CanJump.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class CanJump
+    {
+        public bool Solve(int[] nums)
+        {
+            int canWalk = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (canWalk == 0) 
+                {
+                    return false;
+                }
+
+                canWalk--;
+                canWalk = Math.Max(canWalk, nums[i]);
+            }
+            return true;
+        }
+    }
+}
