334f90a7d5f5dec6d1beafe60793d8396dc87b26
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-23 19:55:06 -0300

    Find all duplicates

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/FindAllDuplicates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class FindAllDuplicates
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var duplicates = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var nextIndex = Math.Abs(nums[i]) - 1;
+
+                if (nums[nextIndex] < 0)
+                {
+                    duplicates.Add(nextIndex + 1);
+                }
+                else
+                {
+                    nums[nextIndex] = nums[nextIndex] * -1;
+                }
+            }
+
+            return duplicates;
+        }
+    }
+}
