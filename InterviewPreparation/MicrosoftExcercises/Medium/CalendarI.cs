1414c0c4bcd0ec9156924d66e90c92ab47e459f3
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-23 19:54:53 -0300

    CalendarI

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CalendarI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    public class CalendarI
+    {
+        private List<int[]> books;
+
+        public CalendarI()
+        {
+            books = new List<int[]>();
+        }
+
+        public bool Book(int start, int end)
+        {
+            foreach (var book in books)
+            {
+                if (!Avaliable(book, start, end))
+                {
+                    return false;
+                }
+            }
+
+            books.Add(new int[] { start, end });
+
+            return true;
+        }
+
+        private bool Avaliable(int[] book, int start, int end)
+        {
+            return !(book[0] < end && start < book[1]);
+        }
+    }
+}
