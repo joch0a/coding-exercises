30421bbcd7d2619da4ae64178cac2380891a29d3
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:30:58 -0300

    Kth in bst

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/KthElementInBST.cs
@@ -0,0 +1,35 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class KthElementInBST
+    {
+        private int currentNumber;
+        private int finalNumber;
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            currentNumber = k;
+
+            CreateInorderBSTList(root);
+
+            return finalNumber;
+        }
+
+        public void CreateInorderBSTList(TreeNode root)
+        {
+            if (root != null && currentNumber != 0)
+            {
+                CreateInorderBSTList(root.left);
+                currentNumber--;
+
+                if (currentNumber == 0)
+                {
+                    finalNumber = root.val;
+                }
+
+                CreateInorderBSTList(root.right);
+            }
+        }
+    }
+}
