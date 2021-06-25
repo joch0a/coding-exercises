af65f9fc27ac475a05fb62cd9d7d1fb648e57f4f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:31:26 -0300

    TriangleNumber

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/TriangleNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class TriangleNumber
+    {
+        public int Solve(int[] nums)
+        {
+            var count = 0;
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                for (int j = i + 1; j < nums.Length - 1; j++)
+                {
+                    var low = j + 1;
+                    var high = nums.Length - 1;
+                    var a = nums[i];
+                    var b = nums[j];
+
+                    while (low <= high)
+                    {
+                        var mid = low + (high - low) / 2;
+                        var c = nums[mid];
+
+                        if (a + b <= c)
+                        {
+                            high = mid - 1;
+                        }
+                        else
+                        {
+                            low = mid + 1;
+                        }
+                    }
+
+                    count += low - j - 1;
+                }
+            }
+
+            return count;
+        }
+    }
+}
