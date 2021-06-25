f5b1024bd04c55b2e073400757673d47a5aa91dc
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:31:08 -0300

    Remove duplicates string

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/RemoveDuplicatesStringLexo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class RemoveDuplicatesStringLexo
+    {
+        public string RemoveDuplicateLetters(string str)
+        {
+            var counts = new int[26];
+            var sb = new StringBuilder();
+            var visited = new HashSet<char>();
+
+            foreach (var currentChar in str)
+            {
+                counts[currentChar - 'a']++;
+            }
+
+            for (var currentIndex = 0; currentIndex < str.Length; currentIndex++)
+            {
+                var currentChar = str[currentIndex];
+                counts[currentChar - 'a']--;
+
+                if (visited.Contains(currentChar))
+                {
+                    continue;
+                }
+
+                while (sb.Length > 0 &&
+                      sb[sb.Length - 1] > currentChar &&
+                      counts[sb[sb.Length - 1] - 'a'] > 0)
+                {
+                    visited.Remove(sb[sb.Length - 1]);
+                    sb.Length--;
+                }
+
+                sb.Append(currentChar);
+                visited.Add(currentChar);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
