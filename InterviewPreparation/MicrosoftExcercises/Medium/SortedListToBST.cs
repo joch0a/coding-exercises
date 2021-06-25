46e50aa67b71c62e3e8d96668d87e76c66203a70
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:31:15 -0300

    Sorted to BST

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/SortedListToBST.cs
@@ -0,0 +1,47 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class SortedListToBST
+    {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            var list = new List<ListNode>();
+
+            var current = head;
+
+            while (current != null)
+            {
+                list.Add(current);
+
+                current = current.next;
+            }
+
+            var array = list.ToArray();
+
+            var BST = CreateBST(array, 0, array.Length - 1);
+
+            return BST;
+        }
+
+        public TreeNode CreateBST(ListNode[] array, int left, int right)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            var currentElement = left + (right - left) / 2;
+
+            var newNode = new TreeNode(array[currentElement].val);
+
+            newNode.left = CreateBST(array, left, currentElement - 1);
+            newNode.right = CreateBST(array, currentElement + 1, right);
+
+            return newNode;
+        }
+    }
+}
