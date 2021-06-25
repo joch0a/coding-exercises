5f199406c8b93312ac8fb68ba2b722304e1e0afa
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:30:43 -0300

    DetectCycle II

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/DetectCycleII.cs
@@ -0,0 +1,35 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class DetectCycleII
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            do
+            {
+                fast = fast?.next?.next;
+                slow = slow?.next;
+            } while (fast != null && fast?.next != null && fast != slow);
+
+
+            if (fast == null || fast?.next == null)
+            {
+                return null;
+            }
+
+            slow = head;
+
+            while (slow != fast)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            return fast;
+        }
+    }
+}
