25f300d219611ecde704b6fa036fab6d9e99db07
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 23:34:46 -0300

    PeekingIterator

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/PeekingIterator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class PeekingIterator
+    {
+        private IEnumerator<int> _iterator;
+        private bool hasNext;
+
+        // iterators refers to the first element of the array.
+        public PeekingIterator(IEnumerator<int> iterator)
+        {
+            // initialize any member here.
+            _iterator = iterator;
+            hasNext = iterator.Current != null;
+        }
+
+        // Returns the next element in the iteration without advancing the iterator.
+        public int Peek()
+        {
+            return _iterator.Current;
+        }
+
+        // Returns the next element in the iteration and advances the iterator.
+        public int Next()
+        {
+            var current = _iterator.Current;
+
+            hasNext = _iterator.MoveNext();
+
+            return current;
+        }
+
+        // Returns false if the iterator is refering to the end of the array of true otherwise.
+        public bool HasNext()
+        {
+            return hasNext;
+        }
+    }
+}
