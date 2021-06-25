2f57dea475f405ab591f3bb52d3dfc198e0dea4f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-24 22:30:52 -0300

    Kth smallest in matrix

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/KSmallestInMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class KSmallestInMatrix
+    {
+        // Kinda shitty sol
+        public int KthSmallest(int[][] matrix, int k)
+        {
+            var sorted = new int[matrix.Length * matrix[0].Length];
+            var sortedIndex = 0;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    sorted[sortedIndex++] = matrix[i][j];
+                }
+            }
+
+            Array.Sort(sorted);
+
+            return sorted[k - 1];
+        }
+    }
+}
