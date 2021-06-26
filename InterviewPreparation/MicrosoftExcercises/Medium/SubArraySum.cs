7567c49ab98ea1c11f4d9ad632b36cdaa007a366
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-25 22:49:05 -0300

    subarraysum

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/SubArraySum.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class SubArraySum
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            var total = 0;
+            var sum = 0;
+            var accumulatorMap = new Dictionary<int, int>();
+            accumulatorMap.Add(0, 1);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (accumulatorMap.ContainsKey(sum - k))
+                {
+                    total += accumulatorMap[sum - k];
+                }
+                if (!accumulatorMap.ContainsKey(sum))
+                {
+                    accumulatorMap.Add(sum, 0);
+                }
+                accumulatorMap[sum]++;
+            }
+            return total;
+        }
+    }
+}
