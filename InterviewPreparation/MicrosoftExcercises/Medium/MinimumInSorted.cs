1d7a160eb494ddb188a4a70ffd98d59db01c89c6
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-27 14:57:04 -0300

    Minimum in rotated sorted array

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/MinimumInSorted.cs
@@ -0,0 +1,36 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class MinimumInSorted
+    {
+        public int FindMin(int[] nums)
+        {
+            var first = nums[0];
+            var last = nums[nums.Length - 1];
+
+            if (first <= last)
+            {
+                return first;
+            }
+
+            var low = 0;
+            var high = nums.Length - 1;
+
+            while (low < high)
+            {
+                var mid = low + (high - low) / 2;
+                var actual = nums[mid];
+
+                if (actual < nums[high])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return nums[low];
+        }
+    }
+}
