024979a8ff0b0161d74932dc82f1ab10aa020c58
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-27 14:56:48 -0300

    FindClosest

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/FindClosestElements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class FindClosestElements
+    {
+        public IList<int> Solve(int[] arr, int k, int x)
+        {
+            // Initialize binary search bounds
+            int low = 0;
+            int high = arr.Length - k;
+
+            while (low < high)
+            {
+                var mid = low + (high - low) / 2;
+
+                if (x - arr[mid] > arr[mid + k] - x)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            var result = new List<int>();
+
+            for (int i = low; i < low + k; i++)
+            {
+                result.Add(arr[i]);
+            }
+
+            return result;
+        }
+    }
+}
