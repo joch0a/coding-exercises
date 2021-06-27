f9bc204887c3832224ecd9b44efe67297c579225
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-27 14:56:41 -0300

    DistribuiteCoins

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/DistribuiteCoins.cs
@@ -0,0 +1,32 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class DistribuiteCoins
+    {
+        public int Solve(TreeNode root)
+        {
+            var moves = 0;
+
+            DFS(root, ref moves);
+
+            return moves;
+        }
+
+        public int DFS(TreeNode root, ref int moves)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftOverload = DFS(root.left, ref moves);
+            var rightOverload = DFS(root.right, ref moves);
+
+            moves += Math.Abs(leftOverload) + Math.Abs(rightOverload);
+
+            return leftOverload + rightOverload + root.val - 1;
+        }
+    }
+}
