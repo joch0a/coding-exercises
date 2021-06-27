2f5bea6b0a762d1022210c128d1a1c23e5460dc4
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-27 14:56:25 -0300

    BiPartite

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/BiPartite.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class BiPartite
+    {
+        public bool IsBipartite(int[][] graph)
+        {
+            var visited = new Dictionary<int, bool>();
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (!visited.ContainsKey(i))
+                {
+                    queue.Enqueue(i);
+                    bool actualSet = true;
+
+                    while (queue.Count > 0)
+                    {
+                        var queueSize = queue.Count;
+
+                        while (queueSize > 0)
+                        {
+                            var actual = queue.Dequeue();
+
+                            if (visited.ContainsKey(actual))
+                            {
+                                if (visited[actual] != actualSet)
+                                {
+                                    return false;
+                                }
+                            }
+                            else
+                            {
+                                visited.Add(actual, actualSet);
+
+                                foreach (var node in graph[actual])
+                                {
+                                    queue.Enqueue(node);
+                                }
+                            }
+
+                            queueSize--;
+                        }
+
+                        actualSet = !actualSet;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
