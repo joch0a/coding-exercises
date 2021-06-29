ed316903b4840ef1458a5b9ff93988f7ecb20e11
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-06-29 01:20:39 -0300

    Insert Into BST

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/InsertIntoBST.cs
@@ -0,0 +1,46 @@
+﻿using InterviewPreparation.Exercises;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class InsertIntoBST
+    {
+        public TreeNode Solve(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return new TreeNode(val);
+            }
+
+            TreeNode prev = null;
+            var current = root;
+            bool isLeft = false;
+
+            while (current != null)
+            {
+                prev = current;
+
+                if (current.val > val)
+                {
+                    current = current.left;
+                    isLeft = true;
+                }
+                else
+                {
+                    current = current.right;
+                    isLeft = false;
+                }
+            }
+
+            if (isLeft)
+            {
+                prev.left = new TreeNode(val);
+            }
+            else
+            {
+                prev.right = new TreeNode(val);
+            }
+
+            return root;
+        }
+    }
+}
