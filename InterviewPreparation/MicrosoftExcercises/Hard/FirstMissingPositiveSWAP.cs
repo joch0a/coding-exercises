5a1c07dd1b48a0976a7b1de57d88a9ed24a98497
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-07-08 23:01:35 -0300

    first missing positive

---
+++ InterviewPreparation/MicrosoftExcercises/Hard/FirstMissingPositiveSWAP.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Hard
+{
+    class FirstMissingPositiveSWAP
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (nums[i] > 0 && nums[i] - 1 < nums.Length &&
+                      nums[i] != i + 1 &&
+                      nums[i] != nums[nums[i] - 1])
+                {
+                    var tmp = nums[i];
+                    nums[i] = nums[nums[i] - 1];
+                    nums[tmp - 1] = tmp;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    return i + 1;
+                }
+            }
+
+            return nums.Length + 1;
+        }
+    }
+}
