780050cb8c26b17eb564d8be8d99795ad233b9fb
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-08 06:00:13 -0300

    3sum

--- InterviewPreparation/MicrosoftExcercises/Medium/3Sum.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/3Sum.cs
@@ -48,5 +48,65 @@ namespace InterviewPreparation.MicrosoftExcercises.Medium
             }
             return result;
         }
+
+        public IList<IList<int>> ThreeSum2(int[] nums)
+        {
+            Array.Sort(nums);
+            var result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i != 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                var left = i + 1;
+                var right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    var sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int>() { nums[i], nums[left], nums[right] });
+
+                        left++;
+                        right--;
+
+                        while (left < right && left > 0 && nums[left] == nums[left - 1])
+                        {
+                            left++;
+                        }
+
+                        while (left < right && right < nums.Length - 1 && nums[right] == nums[right + 1])
+                        {
+                            right--;
+                        }
+                    }
+                    else if (sum > 0)
+                    {
+                        right--;
+
+                        while (left < right && right < nums.Length - 1 && nums[right] == nums[right + 1])
+                        {
+                            right--;
+                        }
+                    }
+                    else
+                    {
+                        left++;
+
+                        while (left < right && left > 0 && nums[left] == nums[left - 1])
+                        {
+                            left++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
