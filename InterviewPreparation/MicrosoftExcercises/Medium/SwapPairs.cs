f3610ca5ff2aed61f022ef69fc0295afa48cbf87
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-08 00:39:26 -0300

    swap pairs

--- InterviewPreparation/MicrosoftExcercises/Medium/SwapPairs.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/SwapPairs.cs
@@ -26,5 +26,44 @@ namespace InterviewPreparation.MicrosoftExcercises.Medium
 
             return head;
         }
+
+        public ListNode Solve2(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            var dummy = new ListNode();
+            var last = dummy;
+            ListNode prev = null;
+            var current = head;
+            var index = 1;
+
+            dummy.next = head;
+
+            while (current != null)
+            {
+                if (index % 2 == 0)
+                {
+                    var tmp = current.next;
+
+                    current.next = prev;
+                    prev.next = tmp;
+                    last.next = current;
+                    last = prev;
+
+                    current = tmp;
+                }
+                else
+                {
+                    prev = current;
+                    current = current.next;
+                }
+
+                index++;
+            }
+
+            return dummy.next;
+        }
     }
 }
