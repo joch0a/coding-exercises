d788851b09d536559175c33723d4e8a2b36b4d52
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-07 23:52:33 -0300

    remove comments

--- InterviewPreparation/MicrosoftExcercises/Medium/RemoveCommnets.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/RemoveCommnets.cs
@@ -45,5 +45,58 @@ namespace InterviewPreparation.MicrosoftExcercises.Medium
 
             return result;
         }
+
+        public IList<string> RemoveComments2(string[] source)
+        {
+            var result = new List<string>();
+            bool isComment = false;
+            var sb = new StringBuilder();
+
+            foreach (var line in source)
+            {
+                if (!isComment)
+                {
+                    sb = new StringBuilder();
+                }
+
+                int i = 0;
+
+                while (i < line.Length)
+                {
+                    if (!isComment && !(line[i] == '/' && i + 1 < line.Length && (line[i + 1] == '/' || line[i + 1] == '*')))
+                    {
+                        sb.Append(line[i]);
+                        i++;
+                    }
+                    else if (!isComment && line[i] == '/' && i + 1 < line.Length && line[i + 1] == '/')
+                    {
+                        i = line.Length;
+                    }
+                    else if (!isComment && line[i] == '/' && i + 1 < line.Length && line[i + 1] == '*')
+                    {
+                        isComment = true;
+                        i++;
+                        i++;
+                    }
+                    else if (isComment && !(line[i] == '*' && i + 1 < line.Length && line[i + 1] == '/'))
+                    {
+                        i++;
+                    }
+                    else if (isComment && line[i] == '*' && i + 1 < line.Length && line[i + 1] == '/')
+                    {
+                        isComment = false;
+                        i++;
+                        i++;
+                    }
+                }
+
+                if (sb.Length > 0 && !isComment)
+                {
+                    result.Add(sb.ToString());
+                }
+            }
+
+            return result;
+        }
     }
 }
