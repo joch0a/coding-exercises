672da22a5340db6717aa50a0d450488e3f16e859
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-08 15:58:25 -0300

    multiply string review

--- InterviewPreparation/MicrosoftExcercises/Medium/MultiplyStrings.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/MultiplyStrings.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace InterviewPreparation.MicrosoftExcercises.Medium
 {
@@ -42,6 +43,53 @@ namespace InterviewPreparation.MicrosoftExcercises.Medium
             }
 
             return sb.ToString();
+        }
+
+        public string Multiply2(string num1, string num2)
+        {
+            if (num1 == "0" || num2 == "0")
+            {
+                return "0";
+            }
+
+            var tmp = new int[num1.Length + num2.Length];
+
+            for (int i = 0; i < num1.Length; i++)
+            {
+                var n1 = (int)(num1[i] - '0');
+
+                for (int j = 0; j < num2.Length; j++)
+                {
+                    var n2 = (int)(num2[j] - '0');
+
+                    tmp[i + j + 1] += n1 * n2;
+                }
+            }
+
+            for (int i = tmp.Length - 1; i > 0; i--)
+            {
+                var sum = tmp[i];
+
+                tmp[i] = sum % 10;
+                tmp[i - 1] += sum / 10;
+            }
+
+            var index = 0;
+
+            while (index < tmp.Length && tmp[index] == 0)
+            {
+                index++;
+            }
+
+            var size = Math.Max(1, tmp.Length - index);
+            var charArray = new char[size];
+
+            for (int i = index; i < tmp.Length; i++)
+            {
+                charArray[i - index] = (char)(tmp[i] + '0');
+            }
+
+            return new string(charArray);
         }
     }
 }
