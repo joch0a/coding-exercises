ca694e80368f5cd2dfcdf7f49a33ebdead77bcfe
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-10 20:34:33 -0300

    coin change 2

--- InterviewPreparation/MicrosoftExcercises/Medium/CoinChangeII.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/CoinChangeII.cs
@@ -18,5 +18,35 @@
 
             return dp[amount];
         }
+
+        public int ChangeReview(int amount, int[] coins)
+        {
+            var M = coins.Length + 1;
+            var N = amount + 1;
+
+            var dp = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                dp[i, 0] = 1;
+            }
+
+            for (int i = 1; i < M; i++)
+            {
+                var coin = coins[i - 1];
+
+                for (int j = 0; j < coin && j < N; j++)
+                {
+                    dp[i, j] = dp[i - 1, j];
+                }
+
+                for (int j = coin; j < N; j++)
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - coin];
+                }
+            }
+
+            return dp[M - 1, N - 1];
+        }
     }
 }
