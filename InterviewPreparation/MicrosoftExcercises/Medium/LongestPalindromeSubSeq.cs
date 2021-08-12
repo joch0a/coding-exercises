9e0900d06762b8f18e896c9abb63014d7bd077d0
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-12 09:16:18 -0300

    longest palindrome

--- InterviewPreparation/MicrosoftExcercises/Medium/LongestPalindromeSubSeq.cs
+++ InterviewPreparation/MicrosoftExcercises/Medium/LongestPalindromeSubSeq.cs
@@ -60,5 +60,45 @@ namespace InterviewPreparation.MicrosoftExcercises.Medium
 
             return dp[n - 1, n - 1];
         }
+
+        public int LongestPalindromeSubseq2(string s)
+        {
+            var cache = new int?[s.Length + 1, s.Length + 1];
+
+            return LongestPalindromeSubseqReview(s, 0, s.Length - 1, cache);
+        }
+
+        public int LongestPalindromeSubseqReview(string s, int i, int j, int?[,] cache)
+        {
+            if (i > j)
+            {
+                return 0;
+            }
+
+            if (i == j)
+            {
+                return 1;
+            }
+
+            if (cache[i, j] != null)
+            {
+                return cache[i, j].Value;
+            }
+
+            var result = 0;
+
+            if (s[i] == s[j])
+            {
+                result = LongestPalindromeSubseqReview(s, i + 1, j - 1, cache) + 2;
+            }
+            else
+            {
+                result = Math.Max(LongestPalindromeSubseqReview(s, i + 1, j, cache), LongestPalindromeSubseqReview(s, i, j - 1, cache));
+            }
+
+            cache[i, j] = result;
+
+            return result;
+        }
     }
 }
