fed5b86ad9c17ffca3b88148bd6d45e69d2c51b6
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-12 09:16:11 -0300

    subarray sum

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CheckSubarraySum.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class CheckSubarraySum
+    {
+        public bool Solve(int[] nums, int k)
+        {
+            var indexes = new Dictionary<int, int>()
+        {
+            {0, -1}
+        };
+            var sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum = (sum + nums[i]) % k;
+
+                if (indexes.ContainsKey(sum))
+                {
+                    var index = indexes[sum];
+
+                    if (i - index > 1)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    indexes.Add(sum, i);
+                }
+            }
+
+            return false;
+        }
+    }
+}
