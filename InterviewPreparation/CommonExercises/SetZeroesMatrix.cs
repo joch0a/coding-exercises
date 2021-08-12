6596e504a3b4f41de06b5e7f01d46422c27d7fbd
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-12 09:15:50 -0300

    zeroes

--- InterviewPreparation/CommonExercises/SetZeroesMatrix.cs
+++ InterviewPreparation/CommonExercises/SetZeroesMatrix.cs
@@ -37,5 +37,93 @@ namespace InterviewPreparation.Exercises
                 }
             }
         }
+
+        public void SetZeroesReview(int[][] matrix)
+        {
+            var colZero = false;
+            var rowZero = false;
+
+            //col check
+            for (int i = 0; i < matrix.Length && !colZero; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    colZero = true;
+                }
+            }
+
+            //row check
+            for (int j = 0; j < matrix[0].Length && !rowZero; j++)
+            {
+                if (matrix[0][j] == 0)
+                {
+                    rowZero = true;
+                }
+            }
+
+            //create marks
+
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                for (int j = 1; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+
+            //vertical flood
+
+            for (int j = 1; j < matrix[0].Length; j++)
+            {
+                if (matrix[0][j] == 0)
+                {
+                    for (int i = 0; i < matrix.Length; i++)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+
+            //horizontal flood
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    for (int j = 0; j < matrix[i].Length; j++)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            if (matrix[0][0] == 0)
+            {
+                for (int j = 1; j < matrix[0].Length; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            if (colZero)
+            {
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+
+            if (rowZero)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+        }
     }
 }
