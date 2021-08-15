39fbeea3501df19069e191a333d4da5a83e020c5
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-15 17:22:55 -0300

    celebrity

---
+++ InterviewPreparation/MicrosoftExcercises/Premium/FindCelebrity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Premium
+{
+    class FindCelebrity
+    {
+        public int Solve(int n)
+        {
+            var candidate = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Knows(candidate, i))
+                {
+                    candidate = i;
+                }
+            }
+
+            return CheckCelebrity(n, candidate);
+        }
+
+        private bool Knows(int candidate, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CheckCelebrity(int totalCandidates, int candidate)
+        {
+            for (int i = 0; i < totalCandidates; i++)
+            {
+                if (i == candidate)
+                {
+                    continue;
+                }
+
+                if (!Knows(i, candidate) || Knows(candidate, i))
+                {
+                    return -1;
+                }
+            }
+
+            return candidate;
+        }
+    }
+}
