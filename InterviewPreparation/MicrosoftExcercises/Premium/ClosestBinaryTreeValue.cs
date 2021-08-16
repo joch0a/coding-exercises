8b204445b39c6e3b27beefce283d2145acc899ac
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-16 20:00:00 -0300

    closest binary tree value

---
+++ InterviewPreparation/MicrosoftExcercises/Premium/ClosestBinaryTreeValue.cs
@@ -0,0 +1,20 @@
+﻿using InterviewPreparation.Exercises;
+using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Premium
+{
+    class ClosestBinaryTreeValue
+    {
+        public int ClosestValue(TreeNode root, double target)
+        {
+            int val, closest = root.val;
+            while (root != null)
+            {
+                val = root.val;
+                closest = Math.Abs(val - target) < Math.Abs(closest - target) ? val : closest;
+                root = target < root.val ? root.left : root.right;
+            }
+            return closest;
+        }
+    }
+}
