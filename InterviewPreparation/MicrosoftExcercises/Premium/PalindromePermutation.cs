8900a6463f43f6bc93eee3ed0105fe4bc1494145
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-17 08:31:17 -0300

    palindrome permutation

---
+++ InterviewPreparation/MicrosoftExcercises/Premium/PalindromePermutation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Premium
+{
+    class PalindromePermutation
+    {
+        public bool CanPermute(string s)
+        {
+            var bucket = new int[26];
+
+            foreach (var c in s)
+            {
+                bucket[c - 'a']++;
+            }
+
+            var oddFound = false;
+
+            foreach (var item in bucket)
+            {
+                if (item % 2 == 1)
+                {
+                    if (oddFound)
+                    {
+                        return false;
+                    }
+
+                    oddFound = true;
+                }
+            }
+
+            return true;
+        }
+
+        public bool CanPermutePalindrome(string s)
+        {
+            var hs = new HashSet<char>();
+
+            foreach (var c in s)
+            {
+                if (hs.Contains(c))
+                {
+                    hs.Remove(c);
+                }
+                else
+                {
+                    hs.Add(c);
+                }
+            }
+
+            return hs.Count < 2;
+        }
+    }
+}
