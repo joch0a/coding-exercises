b123e37b94d3cfcc8384aa1923cda8c650e94374
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-08-18 20:39:06 -0300

    reverse words

---
+++ InterviewPreparation/MicrosoftExcercises/Premium/ReverseWords3.cs
@@ -0,0 +1,33 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Premium
+{
+    class ReverseWords3
+    {
+        public void ReverseWords(char[] s)
+        {
+            Reverse(s, 0, s.Length - 1);
+            var index = 0;
+            var left = 0;
+            while (index <= s.Length)
+            {
+                while (index < s.Length && s[index] != ' ')
+                {
+                    index++;
+                }
+                Reverse(s, left, index - 1);
+                index++;
+                left = index;
+            }
+        }
+        private void Reverse(char[] s, int left, int right)
+        {
+            while (left < right)
+            {
+                var aux = s[left];
+                s[left] = s[right];
+                s[right] = aux;
+                left++;
+                right--;
+            }
+        }
+    }
+}
