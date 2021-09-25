d66f278f92cd1d1978ae665bf5b935cc3e3c63ed
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-25 19:53:29 -0300

    total anagram

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/TotalAnagrams.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class TotalAnagrams
+    {
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            if (p.Length > s.Length)
+            {
+                return new List<int>();
+            }
+
+            var freq = BuildFrequency(p);
+            var count = p.Length;
+            var totalAnagrams = new List<int>();
+            var window = p.Length;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (freq[s[i] - 'a'] > 0)
+                {
+                    count--;
+                }
+
+                freq[s[i] - 'a']--;
+
+                if (i >= window)
+                {
+                    freq[s[i - window] - 'a']++;
+
+                    if (freq[s[i - window] - 'a'] > 0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    totalAnagrams.Add(i - window + 1);
+                }
+            }
+
+            return totalAnagrams;
+        }
+
+        private int[] BuildFrequency(string p)
+        {
+            var freq = new int[26];
+
+            foreach (var c in p)
+            {
+                freq[c - 'a']++;
+            }
+
+            return freq;
+        }
+    }
+}
