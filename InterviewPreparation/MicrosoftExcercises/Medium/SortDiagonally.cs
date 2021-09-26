a87d7bf88bbf69d57fef94b57540d5366eab300b
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-26 16:25:20 -0300

    sort diagonally

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/SortDiagonally.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class SortDiagonally
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            var M = mat.Length;
+            var N = mat[0].Length;
+
+            var diagonals = new Dictionary<int, IList<int>>();
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    var currentDiagonal = GetDiagonal(i, j);
+
+                    if (!diagonals.ContainsKey(currentDiagonal))
+                    {
+                        diagonals.Add(currentDiagonal, new List<int>());
+                    }
+
+                    diagonals[currentDiagonal].Add(mat[i][j]);
+                }
+            }
+
+            foreach (var key in diagonals.Keys.ToList())
+            {
+                diagonals[key] = diagonals[key].OrderBy(val => val).ToList();
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    var currentDiagonal = GetDiagonal(i, j);
+
+                    mat[i][j] = diagonals[currentDiagonal].First();
+
+                    diagonals[currentDiagonal].Remove(mat[i][j]);
+                }
+            }
+
+            return mat;
+        }
+
+
+        public int GetDiagonal(int row, int col)
+        {
+            return row - col;
+        }
+    }
+}
