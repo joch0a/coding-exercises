5fd77a016c466b23865876d0d5a656a7859dc65f
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-26 18:43:38 -0300

    Buildings ocean

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/BuildingsWithanOceanView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class BuildingsWithanOceanView
+    {
+        public int[] FindBuildings(int[] arr)
+        {
+            var maxRightSoFar = -1;
+            var stack = new Stack<int>();
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (maxRightSoFar >= arr[i])
+                {
+                    continue;
+                }
+
+                maxRightSoFar = arr[i];
+
+                stack.Push(i);
+            }
+
+            var result = new int[stack.Count];
+            var index = 0;
+
+            while (stack.Count > 0)
+            {
+                result[index++] = stack.Pop();
+            }
+
+            return result;
+        }
+    }
+}
