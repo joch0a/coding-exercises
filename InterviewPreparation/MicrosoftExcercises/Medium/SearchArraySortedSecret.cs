273cd44931d435db40c7d96a6f687ab3495fe7a7
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-27 23:09:13 -0300

    search array sorted

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/SearchArraySortedSecret.cs
@@ -0,0 +1,64 @@
+﻿namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class SearchArraySortedSecret
+    {
+        public int Search(ArrayReader reader, int target)
+        {
+            var low = 0;
+            var high = GetBound(reader);
+
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                var midElement = reader.Get(mid);
+
+                if (midElement == target)
+                {
+                    return mid;
+                }
+                else if (midElement > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private int GetBound(ArrayReader reader)
+        {
+            var low = 0;
+            var high = 10000;
+
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                var midElement = reader.Get(mid);
+
+                if (midElement != int.MaxValue && reader.Get(mid + 1) == int.MaxValue)
+                {
+                    return mid;
+                }
+                else if (midElement == int.MaxValue)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public class ArrayReader
+        {
+            public int Get(int i) => 1;
+        }
+    }
+}
---
+++ InterviewPreparation/MicrosoftExcercises/Medium/WebCrawler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class WebCrawler
+    {
+        public IList<string> Crawl(string startUrl, HtmlParser htmlParser)
+        {
+            var crawled = new List<string>();
+            var queue = new Queue<string>();
+            var visited = new HashSet<string>();
+            var hostName = GetHostName(startUrl);
+
+            Console.WriteLine(hostName);
+
+            queue.Enqueue(startUrl);
+
+            while (queue.Count > 0)
+            {
+                var actual = queue.Dequeue();
+
+                if (!visited.Contains(actual))
+                {
+                    visited.Add(actual);
+
+                    if (GetHostName(actual) == hostName)
+                    {
+                        crawled.Add(actual);
+
+                        foreach (var node in htmlParser.GetUrls(actual))
+                        {
+                            queue.Enqueue((string)node);
+                        }
+                    }
+                }
+            }
+
+            return crawled;
+        }
+
+        private string GetHostName(string url)
+        {
+            var i = 0;
+            var sb = new StringBuilder();
+
+            if (url[0] == 'h')
+            {
+                while (url[i] != '/')
+                {
+                    i++;
+                }
+
+                while (url[i] == '/')
+                {
+                    i++;
+                }
+            }
+
+            for (int index = i; i < url.Length && url[i] != '/'; i++)
+            {
+                sb.Append(url[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
