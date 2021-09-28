05d797a3d6dd7bd07b38cfdce8ffe99cf6968457
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-27 23:08:50 -0300

    count squares

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/CountSquares.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class CountSquares
+    {
+        public int Solve(int[][] matrix)
+        {
+            var M = matrix.Length;
+            var N = matrix[0].Length;
+            var dp = new int[M + 1, N + 1];
+            var total = 0;
+
+            for (int i = 1; i <= M; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    if (matrix[i - 1][j - 1] == 1)
+                    {
+                        dp[i, j] = Math.Min(
+                                        Math.Min(dp[i - 1, j - 1], dp[i - 1, j]),
+                                        dp[i, j - 1]) + 1;
+
+                        total += dp[i, j];
+                    }
+                }
+            }
+
+            return total;
+        }
+    }
+}
