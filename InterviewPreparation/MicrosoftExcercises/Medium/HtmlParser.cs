8c7cbec4fe507a895e30de80a6120eb2ae751076
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-27 23:08:56 -0300

    html parser

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/HtmlParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    public class HtmlParser
+    {
+        internal IEnumerable<object> GetUrls(string actual)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
