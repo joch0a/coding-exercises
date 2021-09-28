ec1696decd5373aa8222af32fb6dc312e8d1e93c
Jose Ochoa <ochoajoseignacio.96@gmail.com>
2021-09-27 23:09:04 -0300

    remove all ocurrences

---
+++ InterviewPreparation/MicrosoftExcercises/Medium/RemoveAllOcurrences.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace InterviewPreparation.MicrosoftExcercises.Medium
+{
+    class RemoveAllOcurrences
+    {
+        public string RemoveOccurrences(string s, string part)
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result.Append(s[i]);
+
+                if (result.Length >= part.Length &&
+                   result.ToString().Substring(result.Length - part.Length) == part)
+                {
+                    result.Length -= part.Length;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
